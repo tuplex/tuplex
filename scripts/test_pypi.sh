27362ac093cfa74ff94d947a7524e9e0853b3093
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-07-06 17:26:16 -0400

    script, bash subsitution not wokring on gh actions

---
+++ scripts/test_pypi.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# checks out tuplex from test.pypi
+python3 --version
+cat ./scripts/dev.version
+python3 -m pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple "tuplex==$(cat ./scripts/dev.version)"
+python3 -c 'import tuplex; c = tuplex.Context()'

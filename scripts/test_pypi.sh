aef54094b977059fea6bff793a3991cf7f904b4b
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-07-08 22:43:47 -0400

    [CI] Timestamp based dev versions (#6)
    Fix CI to create timestamp based dev versions to avoid conflicts in test.pypi.org
    
    authored-by: Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>

---
+++ scripts/test_pypi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# checks out tuplex from test.pypi
+python3 --version
+cat ./scripts/dev.version
+echo "tuplex==$(cat ./scripts/dev.version)"
+
+# version propagation takes a while...
+sleep 30s
+
+python3 -m pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple "tuplex==$(cat ./scripts/dev.version)"
+python3 -c 'import tuplex; c = tuplex.Context()'

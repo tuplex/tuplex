4f77d6d21163f6423c23f8b152ed319bab1411cd
Malte Schwarzkopf <malte@cs.brown.edu>
2021-08-03 22:51:26 -0400

    Setup on Ubuntu 20.04 should install pip3/libmagic-dev (#17)
    Update Ubuntu 20.04 install requirements scripts.
    
    Details:
    - Setup now installs pip3
    - Setup now installs libmagic-dev
    
    authored-by: Malte Schwarzkopf (malte@cs.brown.edu)

--- scripts/ubuntu2004/install_reqs.sh
+++ scripts/ubuntu2004/install_reqs.sh
@@ -19,8 +19,8 @@ add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 
 apt-get install -y build-essential software-properties-common wget libedit-dev libz-dev \
-  python-yaml pkg-config libssl-dev libcurl4-openssl-dev curl \
-  uuid-dev git libffi-dev \
+  python-yaml python3-pip pkg-config libssl-dev libcurl4-openssl-dev curl \
+  uuid-dev git libffi-dev libmagic-dev \
   doxygen doxygen-doc doxygen-latex doxygen-gui graphviz \
   libgflags-dev libncurses-dev \
   awscli openjdk-8-jdk libyaml-dev

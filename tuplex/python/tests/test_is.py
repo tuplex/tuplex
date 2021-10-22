1bc95da09015334e68e7cf6bcb092a7f75713332
Yash Gotmare <sgotmare@cs.brown.edu>
2021-10-21 22:29:33 -0400

    update test_is.py with failing test

--- tuplex/python/tests/test_is.py
+++ tuplex/python/tests/test_is.py
@@ -3,6 +3,7 @@ import tuplex
 class TestIs(TestCase):
 
     def setUp(self):
+        self.conf = {"webui.enable": False, "driverMemory": "64MB", "executorMemory": "2MB", "partitionSize": "128KB"}
         self.c = tuplex.Context(webui=False)
 
     def test_boolIsBool(self):
@@ -17,7 +18,13 @@ class TestIs(TestCase):
         res = self.c.parallelize([True, False, False, True]).map(lambda x: x is None).collect()
         self.assertEqual(res, [False] * 4)
 
+    # cannot put None here, because None becomes 
     def test_mixedIsNotNone(self):
+        res = self.c.parallelize([None, None, None]).map(lambda x: x is not None).collect()
+        self.assertEqual(res, [False, False, False])
+
+    # this test fails for none, probably has to do with normal case. 
+    def test_mixedIsNotNone2(self):
         res = self.c.parallelize([None, True, False]).map(lambda x: x is not None).collect()
         self.assertEqual(res, [False, True, True])
 

4ffd04c686d34bdf8783308fa7308ebdefca53d3
Yash Gotmare <sgotmare@cs.brown.edu>
2021-10-22 08:04:13 -0400

    nit

--- tuplex/python/tests/test_is.py
+++ tuplex/python/tests/test_is.py
@@ -18,12 +18,10 @@ class TestIs(TestCase):
         res = self.c.parallelize([True, False, False, True]).map(lambda x: x is None).collect()
         self.assertEqual(res, [False] * 4)
 
-    # cannot put None here, because None becomes 
     def test_mixedIsNotNone(self):
         res = self.c.parallelize([None, None, None]).map(lambda x: x is not None).collect()
         self.assertEqual(res, [False, False, False])
 
-    # this test fails for none, probably has to do with normal case. 
     def test_mixedIsNotNone2(self):
         res = self.c.parallelize([None, True, False]).map(lambda x: x is not None).collect()
         self.assertEqual(res, [False, True, True])

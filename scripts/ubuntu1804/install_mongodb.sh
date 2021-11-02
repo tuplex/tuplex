d029b5876e4479589cd97aea03425b2a75eda38d
leonhards <leonhard_spiegelberg@brown.edu>
2021-11-02 12:03:17 -0400

    added mongodb script

---
+++ scripts/ubuntu1804/install_mongodb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# installs MongoDB instance
+# adapted from https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-18-04-source
+# and https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+# needs sudo
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+apt update
+apt install -y mongodb-org

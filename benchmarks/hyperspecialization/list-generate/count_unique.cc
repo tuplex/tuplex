9db1f3fe81518bbc57991a344effc34ed6db608b
Yash Gotmare <sgotmare@cs.brown.edu>
2021-11-02 18:03:09 -0400

    TODO csv reading

---
+++ benchmarks/hyperspecialization/list-generate/count_unique.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "csvmonkey.hpp"
+
+using namespace csvmonkey;
+
+int main(int argc, char** argv) {
+    const char* path = "";
+    if (argc > 1) {
+        path = argv[1];
+    }
+
+    // MappedFileCursor
+}

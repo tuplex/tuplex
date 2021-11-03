6120e207e413977fde2e8b1b71a9b6914f4ee7cc
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-11-03 15:43:49 -0400

    wip

---
+++ scripts/create_lambda_zip.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# (c) 2021 Tuplex team
+
+# this script creates a deployable AWS Lambda zip package using docker
+
+# check from where script is invoked
+CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+echo "Executing buildwheel script located in $CWD"
+pushd $CWD > /dev/null
+cd .. # go to root of repo
+
+# start code here...
+
+
+# end code here...
+popd > /dev/null

ac3bce501135c4c53d9b41ab4c7e33d30b270d84
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-11-09 10:00:55 -0500

    default update

--- tuplex/python/tuplex/__init__.py
+++ tuplex/python/tuplex/__init__.py
@@ -19,7 +19,11 @@ import tuplex.distributed
 from tuplex.distributed import setup_aws
 
 # for convenience create a dummy function to return a default-configured Lambda context
-def LambdaContext(s3_scratch_dir=tuplex.distributed.default_scratch_dir(), **kwargs):
+def LambdaContext(s3_scratch_dir=None, **kwargs):
+
+    if s3_scratch_dir is None:
+        s3_scratch_dir = tuplex.distributed.default_scratch_dir()
+
     # There's currently a bug in the Lambda backend when transferring local data to S3: The full partition gets transferred,
     # not just what is needed.
     return Context(conf={'backend': 'lambda',

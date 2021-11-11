6627d854346b05f1ff6884dbe7d24f0e1f64e9c5
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-11-10 22:14:11 -0500

    install openssl backed curl

---
+++ scripts/docker/ci/install_curl.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# TODO: CentOS/RHEL does not support AWS SDK. It's triggering a bug in NSS which is the SSL lib used in CentOS/RHEL. Therefore, use a m
+# cf. https://github.com/aws/aws-sdk-cpp/issues/1491
+
+# Steps to solve:
+# 1.) install recent OpenSSL
+# 2.) build Curl against it
+# 3.) Compile AWS SDK with this curl version.
+#cf. https://geekflare.com/curl-installation/ for install guide
+
+CURL_VERSION=7.80.0
+
+cd /tmp && yum update -y && yum install wget gcc openssl-devel -y && \
+wget --no-check-certificate https://curl.se/download/curl-${CURL_VERSION}.tar.gz && tar xf curl-${CURL_VERSION}.tar.gz && \
+cd curl-${CURL_VERSION} && ./configure --with-ssl && make -j 16 && make install && ldconfig
+

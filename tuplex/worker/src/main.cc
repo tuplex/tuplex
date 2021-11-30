9009b33b757eed306cca0d6bcb7012ae9d837e22
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-11-30 14:25:30 -0500

    worker process infrastructure

--- tuplex/worker/src/main.cc
+++ tuplex/worker/src/main.cc
@@ -3,17 +3,66 @@
 //
 
 #include <main.h>
+#include <lyra/lyra.hpp>
+
+#include <Logger.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+int initLogging(const std::string& logPath) {
+    using namespace std;
+    using namespace tuplex;
+    if(!logPath.empty()) {
+        try {
+            Logger::instance().init({std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>(), std::make_shared<spdlog::sinks::basic_file_sink_mt>(logPath.c_str())});
+        } catch(...) {
+            return 1;
+        }
+    } else {
+        Logger::instance().init();
+    }
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
     using namespace std;
     using namespace tuplex;
 
-    cout<<"Hello world!"<<endl;
+    std::string logPath;
+    bool isDaemon = false; // do not start in daemon mode
+    unsigned int port = 9000; // default port
+    std::string message; // no message to process
+    unsigned int timeout = 10000; // 10s timeout
+
+    // construct CLI
+    auto cli = lyra::cli();
+    cli.add_argument(lyra::opt(logPath, "logPath").name("--log-path").help("path where to store log file"));
+    cli.add_argument(lyra::opt(isDaemon, "daemon").name("-d").name("--daemon").help("start worker as daemon process listening to connections"));
+    cli.add_argument(lyra::opt(port, "port").name("-p").name("--port").help("port on which worker should listen for messages"));
+    cli.add_argument(lyra::opt(message, "message").name("-m").name("--message").help("single message in JSON for worker to process, auto-shutdown after message"));
+    cli.add_argument(lyra::opt(timeout, "timeout").name("-t").name("--timeout").help("if set to non-zero, timeout in ms after which worker will auto-shutdown"));
+
+    auto result = cli.parse({argc, argv});
+    if(!result) {
+        cerr<<"Error parsing command line: "<<result.errorMessage()<<std::endl;
+        return 1;
+    }
+
+    // logPath given? init Logger with filesink!
+    auto rc = initLogging(logPath);
+    if(0 != rc) {
+        cerr<<"Failed to initialize logging"<<endl;
+        return 1;
+    }
+
+    Logger::instance().defaultLogger().info("Starting Tuplex worker process");
 
     // init Worker with default settings
     auto app = make_unique<WorkerApp>(WorkerSettings());
 
-    return app->messageLoop();
-
-    return 0;
+    rc = app->messageLoop();
+    if(0 == rc)
+        Logger::instance().defaultLogger().info("Terminating Tuplex worker process normally with exit code 0.");
+    else
+        Logger::instance().defaultLogger().error("Terminating Tuplex worker process with exit code " + std::to_string(rc));
+    return rc;
 }

22d73f075b53e07bfbe200a3e0fd8650ecf12d77
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-11-30 00:11:33 -0500

    restricting cmake to 3.21 because newer cmake 3.22 breaks aws sdk as reported in https://github.com/aws/aws-sdk-cpp/issues/1820

--- benchmarks/distributed/tuplex/benchmark.sh
+++ benchmarks/distributed/tuplex/benchmark.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # (c) L.Spiegelberg 2020, this file runs the benchmark for Tuplex on AWS Lambda
 
 NUM_RUNS=1
@@ -9,13 +9,15 @@ mkdir -p ${RESULT_DIR}/1000G
 
 echo "running over 100GB"
 
+PYTHON=python3.9
+
 for ((r = 1; r <= NUM_RUNS; r++)); do
 	LOG=${RESULT_DIR}/100G/tuplex-lambda-run-$r.txt
-	python3.7 runtuplex.py --path 's3://tuplex/data/100GB/*.csv' >$LOG 2>$LOG.stderr
+	${PYTHON} runtuplex.py --path 's3://tuplex/data/100GB/*.csv' >$LOG 2>$LOG.stderr
 done
-
+exit 0
 echo "running over 1TB"
 for ((r = 1; r <= NUM_RUNS; r++)); do
         LOG=${RESULT_DIR}/1000G/tuplex-lambda-run-$r.txt
-        python3.7 runtuplex.py --path 's3://tuplex/data/1000GB/*.csv' >$LOG 2>$LOG.stderr
+        ${PYTHON} runtuplex.py --path 's3://tuplex/data/1000GB/*.csv' >$LOG 2>$LOG.stderr
 done

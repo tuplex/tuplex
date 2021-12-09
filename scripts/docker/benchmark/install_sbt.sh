b5cb722466f526831d3bc4c303f8abec7203b306
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2021-12-09 18:15:02 -0500

    fix

--- scripts/docker/benchmark/install_sbt.sh
+++ scripts/docker/benchmark/install_sbt.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get install apt-transport-https curl gnupg -yqq
 echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" |  tee /etc/apt/sources.list.d/sbt.list
 echo "deb https://repo.scala-sbt.org/scalasbt/debian /" |  tee /etc/apt/sources.list.d/sbt_old.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" |  -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
 chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
 apt-get update
 apt-get install sbt

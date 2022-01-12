72b822dae0a369860e00e1460fd58eb96935ba6a
Your Name <you@example.com>
2022-01-12 12:37:47 -0500

    build script fixes

--- scripts/build_wheel_linux.sh
+++ scripts/build_wheel_linux.sh
@@ -21,6 +21,18 @@ export TUPLEX_BUILD_ALL=0
 export CIBW_ARCHS_LINUX=x86_64
 export CIBW_MANYLINUX_X86_64_IMAGE='registry-1.docker.io/tuplex/ci:latest'
 
+
+# check whether lambda zip was build and stored in build-lambda
+TUPLEX_LAMBDA_ZIP=${TUPLEX_LAMBDA_ZIP:-build-lambda/tplxlam.zip}
+
+
+echo "work dir is: $(pwd)"
+if [[ -f "${TUPLEX_LAMBDA_ZIP}" ]]; then
+	echo "Found lambda runner ${TUPLEX_LAMBDA_ZIP}, adding to package"
+	mkdir -p tuplex/other
+	cp ${TUPLEX_LAMBDA_ZIP} tuplex/other/tplxlam.zip
+fi
+
 export CIBW_ENVIRONMENT="TUPLEX_LAMBDA_ZIP='./tuplex/other/tplxlam.zip' CMAKE_ARGS='-DBUILD_WITH_ORC=ON' LD_LIBRARY_PATH=/usr/local/lib:/opt/lib"
 
 # Use the following line to build only python3.9 wheel
@@ -39,6 +51,9 @@ export CIBW_SKIP="*-musllinux_*"
 
 export CIBW_BUILD_VERBOSITY=3
 export CIBW_PROJECT_REQUIRES_PYTHON=">=3.7"
+
+
+
 
 cibuildwheel --platform linux .
 

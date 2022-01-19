bea800668aab20eaa34cec8ae1f876b28d2718d3
Ben Givertz <bgivertz@gmail.com>
2022-01-19 18:17:41 -0500

    Revert "PR Comments"
    This reverts commit 4bca0ec81a48c3cc523eeec35d2a37ac4a453a03.

--- tuplex/test/wrappers/WrapperTest.cc
+++ tuplex/test/wrappers/WrapperTest.cc
@@ -2410,8 +2410,6 @@ TEST_F(WrapperTest, MixedTypesIsWithNone) {
     PyList_SetItem(listObj, 6, PyList_New(0)); // empty list
     PyList_SetItem(listObj, 7, PyDict_New()); // empty dict
 
-    Py_IncRef(listObj);
-
     auto ref = vector<bool>{true, false, false, false, false, false, false, false};
 
     {

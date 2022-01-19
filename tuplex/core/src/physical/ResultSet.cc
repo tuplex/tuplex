7777886c5b005718d86100f9430d3a8af66fbe09
Ben Givertz <bgivertz@gmail.com>
2022-01-19 16:37:12 -0500

    PR Comments

--- tuplex/core/src/physical/ResultSet.cc
+++ tuplex/core/src/physical/ResultSet.cc
@@ -21,9 +21,7 @@ namespace tuplex {
         for(const auto &p : partitions)
             _partitions.push_back(p);
 
-        for (const auto &elt : pyobjects) {
-            _pyobjects.push_back(elt);
-        }
+        _pyobjects = std::deque<std::tuple<size_t, PyObject*>>(pyobjects.begin(), pyobjects.end());
         _exceptions = exceptions;
         _partitionToExceptionsMap = partitionToExceptionsMap;
         _curRowCounter = 0;

e6bae6e0434bf2a56880139b4fb2544db567a654
Ben Givertz <bgivertz@gmail.com>
2022-01-19 18:17:41 -0500

    Revert "PR Comments"
    This reverts commit 6ada257669ba240ef96ff1eec3af4ad1eb8b42cc.

--- tuplex/test/wrappers/WrapperTest.cc
+++ tuplex/test/wrappers/WrapperTest.cc
@@ -2410,7 +2410,7 @@ TEST_F(WrapperTest, MixedTypesIsWithNone) {
     PyList_SetItem(listObj, 6, PyList_New(0)); // empty list
     PyList_SetItem(listObj, 7, PyDict_New()); // empty dict
 
-    auto ref = vector<bool>{true, false, false, false, false, false, false, false};
+    auto ref = vector<bool>{true, false, false, false, false, false, false, false, false};
 
     {
         auto list = boost::python::list(boost::python::handle<>(listObj));

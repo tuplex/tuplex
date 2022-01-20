c965f862e1d0533e2cc2f0b0bd17e423f29c7d15
Ben Givertz <bgivertz@gmail.com>
2022-01-20 12:49:52 -0500

    Documentation for exception processing and mapping

--- tuplex/core/include/ExceptionInfo.h
+++ tuplex/core/include/ExceptionInfo.h
@@ -12,10 +12,19 @@
 #define TUPLEX_EXCEPTIONINFO_H
 
 namespace tuplex {
+    /*!
+     * Struct to hold information that maps input partitions to input exceptions that occur within them.
+     *
+     * Explanation:
+     * Each input partition is passed the same vector of all input exceptions that occured during data parallelization
+     * or caching. Thus, each input partition is must know how many input exceptions occur in its partition, the index
+     * of the input exception partition where its first exception occurs, and the offset into that partition where the
+     * first exception occurs. These values are held in this struct and each input partition is mapped to an ExceptionInfo.
+     */
     struct ExceptionInfo {
-        size_t numExceptions;
-        size_t exceptionIndex;
-        size_t exceptionOffset;
+        size_t numExceptions; //! number of exceptions that occur within a single input partition
+        size_t exceptionIndex; //! index into a vector of input exception partitions that holds the first input exception
+        size_t exceptionOffset; //! offset into the first input exception partition where the first exception occurs.
 
         ExceptionInfo() :
             numExceptions(0),

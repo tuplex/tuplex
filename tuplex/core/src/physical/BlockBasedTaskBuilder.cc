ea16070946ef2f90fe9fde321892274e4c801fd9
Ben Givertz <bgivertz@gmail.com>
2022-01-24 11:07:59 -0500

    [FEATURE] Parallelize support for normal case violations (#57)
    This commit adds support for non-normal case conforming rows in parallelize. I.e., arbitrary python objects can now be parallelized and will get handled in the fallback path accordingly. Adds support for parallelize with normal case violations and file input schema violations as well.
    
    Details:
    - Add auto unpack option for dicts in parallelize
    - Update exceptions (row numbers) when filters are present
    - Add new ExceptionSourceTaskBuilder to process partitions where also partitions with input exceptions exist
    - Add Documentation for exception processing and mapping
    
    authored-by: Ben Givertz <ben_givertz@brown.edu?
    Co-authored-by: Leonhard Spiegelberg <leonhard@brown.edu>

--- tuplex/core/src/physical/BlockBasedTaskBuilder.cc
+++ tuplex/core/src/physical/BlockBasedTaskBuilder.cc
@@ -50,6 +50,47 @@ namespace tuplex {
             return _func;
         }
 
+        llvm::Function* BlockBasedTaskBuilder::createFunctionWithExceptions() {
+            using namespace llvm;
+            using namespace std;
+
+            auto& context = env().getContext();
+            FunctionType* read_block_type = FunctionType::get(env().i64Type(), {env().i8ptrType(),
+                                                                                env().i8ptrType(),
+                                                                                env().i64Type(),
+                                                                                env().i8ptrType()->getPointerTo(0),
+                                                                                env().i64Type()->getPointerTo(0),
+                                                                                env().i64Type(),
+                                                                                env().i64Type()->getPointerTo(0),
+                                                                                env().i64Type()->getPointerTo(0),
+                                                                                env().getBooleanType()}, false);
+            // create function and set argNames
+            Function* read_block_func = Function::Create(read_block_type, Function::ExternalLinkage, _desiredFuncName, env().getModule().get());
+
+            std::vector<llvm::Argument*> args;
+            for(auto& arg : read_block_func->args()) {
+                args.push_back(&arg);
+            }
+
+            // rename args
+            vector<string> argNames{"userData",
+                                    "inPtr",
+                                    "inSize",
+                                    "expPtrs",
+                                    "expPtrSizes",
+                                    "numExps",
+                                    "outNormalRowCount",
+                                    "outBadRowCount",
+                                    "ignoreLastRow"};
+            for(int i = 0; i < argNames.size(); ++i) {
+                args[i]->setName(argNames[i]);
+                _args[argNames[i]] = args[i];
+            }
+
+            _func = read_block_func;
+            return _func;
+        }
+
         void BlockBasedTaskBuilder::setIntermediateInitialValueByRow(const python::Type &intermediateType,
                                                                      const Row &row) {
             _intermediateInitialValue = row;
--- tuplex/test/core/CacheTest.cc
+++ tuplex/test/core/CacheTest.cc
@@ -16,6 +16,101 @@ class CacheTest : public PyTest {
 using namespace tuplex;
 using namespace std;
 
+TEST_F(CacheTest, MergeInOrderWithFilter) {
+    using namespace std;
+    using namespace tuplex;
+
+    auto fileURI = URI(testName + ".csv");
+
+    auto opt = microTestOptions();
+    // enable NullValue Optimization
+    opt.set("tuplex.useLLVMOptimizer", "true");
+    opt.set("tuplex.optimizer.generateParser", "true");
+    opt.set("tuplex.executorCount", "0");
+    opt.set("tuplex.optimizer.mergeExceptionsInOrder", "true");
+    opt.set("tuplex.optimizer.nullValueOptimization", "true");
+    opt.set("tuplex.normalcaseThreshold", "0.6");
+    opt.set("tuplex.resolveWithInterpreterOnly", "false");
+
+    Context c(opt);
+
+    stringstream ss;
+    for (int i = 0; i < 500; ++i) {
+        if (i % 4 == 0) {
+            ss << ",-1\n";
+        } else {
+            ss << to_string(i) << "," << to_string(i) << "\n";
+        }
+    }
+    stringToFile(fileURI, ss.str());
+
+    auto ds_cached = c.csv(fileURI.toPath()).cache();
+
+    auto res = ds_cached.filter(UDF("lambda x, y: y % 3 != 0")).map(UDF("lambda x, y: y")).collectAsVector();
+
+    std::vector<Row> expectedOutput;
+    for (int i = 0; i < 500; ++i) {
+        if (i % 4 == 0) {
+            expectedOutput.push_back(Row(-1));
+        } else if (i % 3 != 0) {
+            expectedOutput.push_back(Row(i));
+        }
+    }
+
+    ASSERT_EQ(expectedOutput.size(), res.size());
+    for (int i = 0; i < expectedOutput.size(); ++i) {
+        EXPECT_EQ(expectedOutput[i].toPythonString(), res[i].toPythonString());
+    }
+}
+
+TEST_F(CacheTest, MergeInOrder) {
+    using namespace std;
+    using namespace tuplex;
+
+    auto fileURI = URI(testName + ".csv");
+
+    auto opt = microTestOptions();
+    // enable NullValue Optimization
+    opt.set("tuplex.useLLVMOptimizer", "true");
+    opt.set("tuplex.optimizer.generateParser", "true");
+    opt.set("tuplex.executorCount", "0");
+    opt.set("tuplex.optimizer.mergeExceptionsInOrder", "true");
+    opt.set("tuplex.optimizer.nullValueOptimization", "true");
+    opt.set("tuplex.normalcaseThreshold", "0.6");
+    opt.set("tuplex.resolveWithInterpreterOnly", "false");
+
+    Context c(opt);
+
+    auto size = 403;
+    auto mod1 = 5;
+    auto mod2 = 6;
+
+    stringstream ss;
+    for (int i = 0; i < size; ++i) {
+        if (i % mod1 == 0 || i % mod2 == 0) {
+            ss << ",,,,-1\n";
+        } else {
+            ss << to_string(i) << "," << to_string(i) << "," << to_string(i) << "," << to_string(i) << "," << to_string(i) << "\n";
+        }
+    }
+
+    stringToFile(fileURI, ss.str());
+
+    auto ds_cached = c.csv(fileURI.toPath()).cache();
+
+    auto res = ds_cached.map(UDF("lambda x: x[4]")).collectAsVector();
+    printRows(res);
+
+    ASSERT_EQ(res.size(), size);
+    for (int i = 0; i < res.size(); ++i) {
+        if (i % mod1 == 0 || i % mod2 == 0) {
+            EXPECT_EQ(res[i].toPythonString(), Row(-1).toPythonString());
+        } else {
+            EXPECT_EQ(res[i].toPythonString(), Row(i).toPythonString());
+        }
+    }
+}
+
 // Note: only this test here fails...
 TEST_F(CacheTest, SimpleCSVLoad) {
     using namespace std;

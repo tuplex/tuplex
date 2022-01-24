782c58bfe45b2b8dec1917ae170e49265e2f2e3c
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-01-24 13:00:56 -0500

    correcting dask for pypy

--- benchmarks/zillow/Z1/rundask.py
+++ benchmarks/zillow/Z1/rundask.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Dask based cleaning script (can be easily modified to pandas/modin/...)
+import platform
 import time
 import sys
 import json
@@ -133,7 +134,13 @@ def runDask(paths, output_path):
 
     df['bedrooms'] = df['facts and features'].apply(extractBd, meta=('facts and features', 'str')).astype(int)
     df = df[df['bedrooms'] < 10]
-    df['type'] = df['title'].apply(extractType)
+
+    # correct for PyPy internal error
+    meta_title = None
+    if platform.python_implementation().lower() == 'pypy':
+        meta_title = ('title', 'str')
+
+    df['type'] = df['title'].apply(extractType, meta=meta_title)
     df = df[df['type'] == 'house']
 
     df['zipcode'] = df['postal_code'].apply(lambda zc: '%05d' % int(zc), meta=('postal_code', 'str'))
@@ -141,7 +148,7 @@ def runDask(paths, output_path):
     df['city'] = df['city'].apply(lambda x: x[0].upper() + x[1:].lower(), meta=('city', 'str'))
     df['bathrooms'] = df['facts and features'].apply(extractBa, meta=('facts and features', 'str')).astype(int)
     df['sqft'] = df['facts and features'].apply(extractSqft, meta=('facts and features', 'str')).astype(int)
-    df['offer'] = df['title'].apply(extractOffer)
+    df['offer'] = df['title'].apply(extractOffer, meta=meta_title)
 
     df['price'] = df[['price', 'offer', 'facts and features', 'sqft']].apply(extractPrice, axis=1, meta=('facts and features', 'str')).astype(int)
 

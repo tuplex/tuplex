d6379ee0ff3cd26ccfc5d4bb9f7c408878d1e1db
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-02-04 16:16:40 -0500

    multiple threading versions of zillow benchmark

--- benchmarks/distributed/zillow/tuplex/runbenchmark.sh
+++ benchmarks/distributed/zillow/tuplex/runbenchmark.sh
@@ -18,16 +18,56 @@ LAMBDA_MEMORY=10000
 LAMBDA_CONCURRENCY=120
 LAMBDA_THREADS=4
 
-echo "benchmarking Zillow (Z1) over 100G with Tuplex (compiled)"
-OUTPUT_PATH="s3://tuplex-leonhard/experiments/Zillow/Z1/output_compiled"
+echo "benchmarking Zillow (Z1) over 100G with Tuplex (compiled, 1 thread)"
+OUTPUT_PATH="s3://tuplex-leonhard/experiments/Zillow/Z1/output_compiled_1x"
+LAMBDA_THREADS=1
 for ((r = 1; r <= NUM_RUNS; r++)); do
   echo "Run $r/${NUM_RUNS}"
-  LOG="${RESDIR}/tuplex-compiled-run-$r.txt"
+  LOG="${RESDIR}/tuplex-compiled-1x-run-$r.txt"
   timeout $TIMEOUT ${PYTHON} runtuplex.py -m "compiled" --path $INPUT_PATH --output-path $OUTPUT_PATH \
                                           --scratch-dir $SCRATCH_DIR --lambda-memory $LAMBDA_MEMORY \
                                           --lambda-concurrency $LAMBDA_CONCURRENCY \
                                           --lambda-threads $LAMBDA_THREADS >$LOG 2>$LOG.stderr
-  cp experiment.log "${RESDIR}/tuplex-compiled-run-$r.log.txt"
+  cp experiment.log "${RESDIR}/tuplex-compiled-1x-run-$r.log.txt"
+done
+
+echo "benchmarking Zillow (Z1) over 100G with Tuplex (compiled, 2 threads)"
+OUTPUT_PATH="s3://tuplex-leonhard/experiments/Zillow/Z1/output_compiled_2x"
+LAMBDA_THREADS=1
+for ((r = 1; r <= NUM_RUNS; r++)); do
+  echo "Run $r/${NUM_RUNS}"
+  LOG="${RESDIR}/tuplex-compiled-2x-run-$r.txt"
+  timeout $TIMEOUT ${PYTHON} runtuplex.py -m "compiled" --path $INPUT_PATH --output-path $OUTPUT_PATH \
+                                          --scratch-dir $SCRATCH_DIR --lambda-memory $LAMBDA_MEMORY \
+                                          --lambda-concurrency $LAMBDA_CONCURRENCY \
+                                          --lambda-threads $LAMBDA_THREADS >$LOG 2>$LOG.stderr
+  cp experiment.log "${RESDIR}/tuplex-compiled-2x-run-$r.log.txt"
+done
+
+echo "benchmarking Zillow (Z1) over 100G with Tuplex (compiled, 4 threads)"
+OUTPUT_PATH="s3://tuplex-leonhard/experiments/Zillow/Z1/output_compiled_4x"
+LAMBDA_THREADS=4
+for ((r = 1; r <= NUM_RUNS; r++)); do
+  echo "Run $r/${NUM_RUNS}"
+  LOG="${RESDIR}/tuplex-compiled-4x-run-$r.txt"
+  timeout $TIMEOUT ${PYTHON} runtuplex.py -m "compiled" --path $INPUT_PATH --output-path $OUTPUT_PATH \
+                                          --scratch-dir $SCRATCH_DIR --lambda-memory $LAMBDA_MEMORY \
+                                          --lambda-concurrency $LAMBDA_CONCURRENCY \
+                                          --lambda-threads $LAMBDA_THREADS >$LOG 2>$LOG.stderr
+  cp experiment.log "${RESDIR}/tuplex-compiled-4x-run-$r.log.txt"
+done
+
+echo "benchmarking Zillow (Z1) over 100G with Tuplex (compiled, 6 threads)"
+OUTPUT_PATH="s3://tuplex-leonhard/experiments/Zillow/Z1/output_compiled_6x"
+LAMBDA_THREADS=6
+for ((r = 1; r <= NUM_RUNS; r++)); do
+  echo "Run $r/${NUM_RUNS}"
+  LOG="${RESDIR}/tuplex-compiled-6x-run-$r.txt"
+  timeout $TIMEOUT ${PYTHON} runtuplex.py -m "compiled" --path $INPUT_PATH --output-path $OUTPUT_PATH \
+                                          --scratch-dir $SCRATCH_DIR --lambda-memory $LAMBDA_MEMORY \
+                                          --lambda-concurrency $LAMBDA_CONCURRENCY \
+                                          --lambda-threads $LAMBDA_THREADS >$LOG 2>$LOG.stderr
+  cp experiment.log "${RESDIR}/tuplex-compiled-6x-run-$r.log.txt"
 done
 
 echo "benchmarking Zillow (Z1) over 100G with Tuplex (interpreted)"

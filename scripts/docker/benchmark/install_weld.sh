64121ef7532ad2e62bced88051e104842a9475b8
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-02-08 16:19:35 -0500

    script fix

--- scripts/docker/benchmark/install_weld.sh
+++ scripts/docker/benchmark/install_weld.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# check from where script is invoked
+CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
 # Install Rust (without the stupid prompt)
 apt-get update && apt-get install -y curl git build-essential
 
@@ -23,7 +26,7 @@ popd
 # need to fix Makefile -> copy Makefile fix
 export PATH=/opt/llvm-6.0/bin/:$PATH
 export WELD_HOME=/opt/weld
-cp PatchedGrizzlyMakefile.make /opt/weld/python/grizzly/grizzly/Makefile
+cp $CWD/PatchedGrizzlyMakefile.make /opt/weld/python/grizzly/grizzly/Makefile
 
 apt-get update && apt install -y python2 python2-dev && curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py && python2 get-pip.py && pip2 install pandas numpy
 cd /opt/weld/python && cd pyweld && python2 setup.py install && cd .. && cd grizzly && python2 setup.py install

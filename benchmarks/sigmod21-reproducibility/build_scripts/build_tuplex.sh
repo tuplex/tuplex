a5793c8475ebacee843aa73d9ed6b45463180222
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-02-08 21:31:53 -0500

    only build C extension

--- benchmarks/sigmod21-reproducibility/build_scripts/build_tuplex.sh
+++ benchmarks/sigmod21-reproducibility/build_scripts/build_tuplex.sh
@@ -8,7 +8,7 @@ TUPLEX_DIR=/code
 
 cd $TUPLEX_DIR && cd tuplex && mkdir -p build && \
 cd build && \
-cmake -DBUILD_WITH_AWS=OFF -DBUILD_NATIVE=ON -DPYTHON3_VERSION=3.6 -DLLVM_ROOT_DIR=/usr/lib/llvm-9 -DCMAKE_BUILD_TYPE=Release .. && \
+cmake -DBUILD_WITH_AWS=OFF -DBUILD_NATIVE=ON -DPYTHON3_VERSION=3.6 -DLLVM_ROOT_DIR=/usr/lib/llvm-9 -DCMAKE_BUILD_TYPE=Release .. tuplex && \
 make -j16 && \
 cd dist/python/ && \
 python3.6 setup.py install

1693ba9c2d52407fcf6260320d104240b50dd799
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-02-08 21:33:14 -0500

    build fix

--- benchmarks/sigmod21-reproducibility/build_scripts/build_tuplex.sh
+++ benchmarks/sigmod21-reproducibility/build_scripts/build_tuplex.sh
@@ -4,11 +4,15 @@
 export CC=gcc-10
 export CXX=g++-10
 
+
+CPU_COUNT=$(nproc)
+echo "Building using $CPU_COUNT"
+
 TUPLEX_DIR=/code
 
 cd $TUPLEX_DIR && cd tuplex && mkdir -p build && \
 cd build && \
-cmake -DBUILD_WITH_AWS=OFF -DBUILD_NATIVE=ON -DPYTHON3_VERSION=3.6 -DLLVM_ROOT_DIR=/usr/lib/llvm-9 -DCMAKE_BUILD_TYPE=Release .. tuplex && \
-make -j16 && \
+cmake -DBUILD_WITH_AWS=OFF -DBUILD_NATIVE=ON -DPYTHON3_VERSION=3.6 -DLLVM_ROOT_DIR=/usr/lib/llvm-9 -DCMAKE_BUILD_TYPE=Release .. && \
+make -j${CPU_COUNT} tuplex && \
 cd dist/python/ && \
 python3.6 setup.py install

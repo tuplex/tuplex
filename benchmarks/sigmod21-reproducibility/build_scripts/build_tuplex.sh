9772e4d0005678be6d230104afad0ad05a677017
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-02-08 22:01:52 -0500

    fixed to use correct llvm9

--- benchmarks/sigmod21-reproducibility/build_scripts/build_tuplex.sh
+++ benchmarks/sigmod21-reproducibility/build_scripts/build_tuplex.sh
@@ -12,7 +12,7 @@ TUPLEX_DIR=/code
 
 cd $TUPLEX_DIR && cd tuplex && mkdir -p build && \
 cd build && \
-cmake -DBUILD_WITH_AWS=OFF -DBUILD_NATIVE=ON -DPYTHON3_VERSION=3.6 -DLLVM_ROOT_DIR=/usr/lib/llvm-9 -DCMAKE_BUILD_TYPE=Release .. && \
+cmake -DBUILD_WITH_AWS=OFF -DBUILD_NATIVE=ON -DPYTHON3_VERSION=3.6 -DLLVM_ROOT_DIR=/opt/llvm-9.0 -DCMAKE_BUILD_TYPE=Release .. && \
 make -j${CPU_COUNT} tuplex && \
 cd dist/python/ && \
 python3.6 setup.py install

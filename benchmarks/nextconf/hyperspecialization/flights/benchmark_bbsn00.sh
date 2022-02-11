4e1cdc9cb735e54670f1f1fca220becfefdb80f0
Your Name <you@example.com>
2022-02-10 21:14:41 -0500

    more analysis

--- benchmarks/nextconf/hyperspecialization/flights/benchmark_bbsn00.sh
+++ benchmarks/nextconf/hyperspecialization/flights/benchmark_bbsn00.sh
@@ -9,11 +9,28 @@ mkdir -p ${RESDIR}
 # build benchmark
 mkdir -p build && pushd build &&
 cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j$(nproc)
 
 mkdir -p process_row && mv *.so process_row
 # run query using runner
-./runner_orig --path $INPUT_FILE --output_path $RESDIR
-./runner_constant --path $INPUT_FILE --output_path $RESDIR
-./runner_narrow --path $INPUT_FILE --output_path $RESDIR
+NUM_RUNS="${NUM_RUNS:-11}"
+
+BENCHDIR=benchmark_results
+mkdir -p ${BENCHDIR}
+
+for ((r=1; r <= NUM_RUNS; r++)); do
+LOG=${BENCHDIR}/flights-orig-run-$r.txt
+./runner_orig --path $INPUT_FILE --output_path $RESDIR 2>&1 | tee $LOG
+done
+
+for ((r=1; r <= NUM_RUNS; r++)); do
+LOG=${BENCHDIR}/flights-constant-run-$r.txt
+./runner_constant --path $INPUT_FILE --output_path $RESDIR 2>&1 | tee $LOG
+done
+
+for ((r=1; r <= NUM_RUNS; r++)); do
+LOG=${BENCHDIR}/flights-narrow-run-$r.txt
+./runner_narrow --path $INPUT_FILE --output_path $RESDIR 2>&1 | tee $LOG
+done
 
 popd

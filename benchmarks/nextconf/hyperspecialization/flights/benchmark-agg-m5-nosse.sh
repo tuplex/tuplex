ec70829ed04ba88af4b2e87ac86f107b7735e552
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-02-25 12:13:16 -0500

    remove march=native

--- benchmarks/nextconf/hyperspecialization/flights/benchmark-agg-m5-nosse.sh
+++ benchmarks/nextconf/hyperspecialization/flights/benchmark-agg-m5-nosse.sh
@@ -11,8 +11,8 @@ SO_GENERAL=agg_weather_general-nosse.so
 
 #export PATH=/opt/llvm@6/bin:$PATH
 echo "Building shared object..."
-clang++ -shared -fPIC -O3 -msse4.2 -mcx16 -march=native -fno-vectorize -mno-sse -DNDEBUG -o $SO_GENERAL src/agg_query/agg_general.cc
-clang++ -shared -fPIC -O3 -msse4.2 -mcx16 -march=native -fno-vectorize -mno-sse -DNDEBUG -o $SO_SPECIALIZED src/agg_query/agg_specialized.cc
+clang++ -shared -fPIC -O3 -msse4.2 -mcx16 -fno-vectorize -mno-sse -DNDEBUG -o $SO_GENERAL src/agg_query/agg_general.cc
+clang++ -shared -fPIC -O3 -msse4.2 -mcx16 -fno-vectorize -mno-sse -DNDEBUG -o $SO_SPECIALIZED src/agg_query/agg_specialized.cc
 
 echo "FINAL EXE"
 # use here vectorized instructions to make things fast...
@@ -39,13 +39,13 @@ for file in $(ls $ROOT_PATH); do
     for ((r=1; r <= NUM_RUNS; r++)); do
       LOG=${BENCHDIR}/flights-specialized-run-$r-date-${yearmonth}.txt
       OUTPUT=$RESULT_DIR/out-specialized-${yearmonth}.csv
-      ./runner -i ${file} -o $OUTPUT -d ./agg_weather_specialized.so 2>&1 | tee $LOG
+      ./runner -i ${file} -o $OUTPUT -d ./$SO_SPECIALIZED 2>&1 | tee $LOG
     done
 
     for ((r=1; r <= NUM_RUNS; r++)); do
       LOG=${BENCHDIR}/flights-general-run-$r-date-${yearmonth}.txt
       OUTPUT=$RESULT_DIR/out-general-${yearmonth}.csv
-      ./runner -i ${file} -o $OUTPUT -d ./agg_weather_general.so 2>&1 | tee $LOG
+      ./runner -i ${file} -o $OUTPUT -d ./$SO_GENERAL 2>&1 | tee $LOG
     done
   else
     # general case
@@ -53,7 +53,7 @@ for file in $(ls $ROOT_PATH); do
      for ((r=1; r <= NUM_RUNS; r++)); do
         LOG=${BENCHDIR}/flights-general-run-$r-date-${yearmonth}.txt
         OUTPUT=$RESULT_DIR/out-general-${yearmonth}.csv
-        ./runner -i ${file} -o $OUTPUT -d ./agg_weather_general.so 2>&1 | tee $LOG
+        ./runner -i ${file} -o $OUTPUT -d ./$SO_GENERAL 2>&1 | tee $LOG
 
         # mimick specilaized by copying, no difference.
         cp $LOG ${BENCHDIR}/flights-specialized-run-$r-date-${yearmonth}.txt

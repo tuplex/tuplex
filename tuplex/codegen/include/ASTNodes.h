a6bd4408bc2c44aea60cadb7d6c12bddd61c6aba
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-02-28 02:31:12 -0500

    literal folding fix

--- tuplex/codegen/include/ASTNodes.h
+++ tuplex/codegen/include/ASTNodes.h
@@ -72,18 +72,6 @@ namespace tuplex {
         Continue
     };
 
-    inline bool isLiteralASTNode(ASTNode* node) {
-        switch(node->type()) {
-            case ASTNodeType::Boolean:
-            case ASTNodeType::Number:
-            case ASTNodeType::String:
-            case ASTNodeType::None:
-                return true;
-            default:
-                return false;
-        }
-    }
-
 //@TODO: Make sure that cloning also copies over protected attributes!
 
 // base class used to store any node in the AST tree
@@ -145,6 +133,18 @@ namespace tuplex {
 
         inline bool hasAnnotation() const { return _annotation; }
     };
+
+    inline bool isLiteralASTNode(ASTNode* node) {
+        switch(node->type()) {
+            case ASTNodeType::Boolean:
+            case ASTNodeType::Number:
+            case ASTNodeType::String:
+            case ASTNodeType::None:
+                return true;
+            default:
+                return false;
+        }
+    }
 
 
 /**********************************************/

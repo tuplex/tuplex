6cd940973e50273a4fcf84234a8f64fd1a6555f6
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-03-08 14:36:54 -0500

    another cereal try

--- tuplex/core/include/logical/Operators.h
+++ tuplex/core/include/logical/Operators.h
@@ -23,24 +23,4 @@
 #include "UDFOperator.h"
 #include "WithColumnOperator.h"
 
-// CEREAL
-#include "cereal/types/polymorphic.hpp"
-#include "cereal/archives/binary.hpp"
-#include "cereal/archives/portable_binary.hpp"
-
-// not required according to cereal doc, yet register despite
-CEREAL_REGISTER_TYPE_WITH_NAME(tuplex::LogicalOperator, "LogicalOperator");
-CEREAL_REGISTER_TYPE_WITH_NAME(tuplex::FileInputOperator, "FileInputOperator");
-CEREAL_REGISTER_TYPE_WITH_NAME(tuplex::UDFOperator, "UDFOperator");
-CEREAL_REGISTER_TYPE_WITH_NAME(tuplex::MapOperator, "MapOperator");
-
-CEREAL_REGISTER_POLYMORPHIC_RELATION(tuplex::LogicalOperator, tuplex::UDFOperator)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(tuplex::LogicalOperator, tuplex::FileInputOperator)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(tuplex::UDFOperator, tuplex::MapOperator)
-
-// Potentially necessary if no explicit reference
-// to objects in myclasses.cpp will take place
-// from other translation units
-CEREAL_REGISTER_DYNAMIC_INIT(tuplex_classes)
-
 #endif //TUPLEX_OPERATORS_H

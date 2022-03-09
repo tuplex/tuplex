9ef6a9abfa4651a9f7afbc76455270120c2cf56a
leonhards <leonhard_spiegelberg@brown.edu>
2022-03-09 16:02:44 -0500

    building aws sdk cpp from scratch to avoid macos issue

---
+++ scripts/macos/install_aws-sdk-cpp.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "installing AWS SDK from source"
+CPU_CORES=$(sysctl -n hw.physicalcpu)
+
+cd /tmp &&
+  git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp.git &&
+  cd aws-sdk-cpp && git checkout tags/1.9.200 && mkdir build && pushd build &&
+  cmake -DCMAKE_BUILD_TYPE=Release -DUSE_OPENSSL=ON -DENABLE_TESTING=OFF -DENABLE_UNITY_BUILD=ON -DCPP_STANDARD=14 -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="s3;core;lambda;transfer" .. &&
+  make -j${CPU_CORES} &&
+  make install &&
+  popd &&
+  cd - || echo "AWS SDK failed"

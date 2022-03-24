d45375a56a7c72ee2fcc0ef1e9f927139591687d
Ben Givertz <bgivertz@gmail.com>
2022-03-24 10:26:40 -0400

    Commit to merge in order

--- benchmarks/incremental/export_results.py
+++ benchmarks/incremental/export_results.py
@@ -13,17 +13,16 @@ def validate_experiment(compare_path):
         lines = f.read().splitlines()
         return ">>> contents of folders match." in lines
 
-
-def job_times(path):
-    times = []
+def get_metric(path, metric, step):
     with open(path) as f:
         lines = f.read().splitlines()
         ind = lines.index("EXPERIMENTAL RESULTS") + 2
-        lines = lines[ind:ind + 7]
-        for line in lines:
-            times.append(json.loads(line)['jobTime'])
-    return times
-
+        line = lines[ind + step]
+        metrics = json.loads(line)
+        if metric == 'jobTime':
+            return metrics[metric]
+        else:
+            return metrics["stages"][0][metric]
 
 def compare_path(trial, mode):
     return "tuplex-compare-{}{}-ssd-{}.txt".format('out-of-order' if mode == Mode.OUT_OF_ORDER else 'in-order',
@@ -37,7 +36,7 @@ def experiment_path(trial, incremental, mode):
                                               '-commit' if mode == Mode.COMMIT else '',
                                               trial)
 
-def write_total_time_to_file(f, results_path, num_trials, num_steps, mode):
+def write_metric_to_file(f, results_path, num_trials, num_steps, mode, metric):
     header = "{},".format("Out of Order" if mode == Mode.OUT_OF_ORDER else "In Order") + \
              "," * num_trials + "Plain," + \
              "," * num_trials + "Incremental" + \
@@ -58,19 +57,19 @@ def write_total_time_to_file(f, results_path, num_trials, num_steps, mode):
         plain_total = 0
         for trial in range(num_trials):
             plain_path = os.path.join(results_path, experiment_path(trial + 1, False, Mode.OUT_OF_ORDER))
-            plain_times = job_times(plain_path)
+            plain_time = get_metric(plain_path, metric, step)
 
-            plain_total += plain_times[step]
-            line += f"{plain_times[step]},"
+            plain_total += plain_time
+            line += f"{plain_time},"
         line += f"{plain_total / num_trials},"
 
         incremental_total = 0
         for trial in range(num_trials):
             incremental_path = os.path.join(results_path, experiment_path(trial + 1, True, Mode.IN_ORDER))
-            incremental_times = job_times(incremental_path)
+            incremental_time = get_metric(incremental_path, metric, step)
 
-            incremental_total += incremental_times[step]
-            line += f"{incremental_times[step]},"
+            incremental_total += incremental_time
+            line += f"{incremental_time},"
         line += f"{incremental_total / num_trials}"
 
         if mode == Mode.IN_ORDER:
@@ -78,99 +77,30 @@ def write_total_time_to_file(f, results_path, num_trials, num_steps, mode):
             commit_total = 0
             for trial in range(num_trials):
                 commit_path = os.path.join(results_path, experiment_path(trial + 1, True, Mode.COMMIT))
-                commit_times = job_times(commit_path)
+                commit_time = get_metric(commit_path, metric, step)
 
-                commit_total += commit_times[step]
-                line += f"{commit_times[step]},"
+                commit_total += commit_time
+                line += f"{commit_time},"
             line += f"{commit_total / num_trials}\n"
         else:
             line += "\n"
 
         f.write(line)
 
-def export_total_time_experiment(results_path, num_trials, num_steps):
+def export_experiments(results_path, num_trials, num_steps):
     # Validate all experiments
     for i in range(num_trials):
         for mode in Mode:
             validate_path = os.path.join(results_path, compare_path(i + 1, mode))
             assert validate_experiment(validate_path)
 
-    file_path = "total_time_experiment.csv"
+    metrics = ['jobTime', 'fast_path_time_s', 'slow_path_time_s', 'write_output_wall_time_s']
+
+    file_path = "experiments.csv"
     with open(file_path, 'w') as f:
-        write_total_time_to_file(f, results_path, num_trials, num_steps, Mode.OUT_OF_ORDER)
-        write_total_time_to_file(f, results_path, num_trials, num_steps, Mode.IN_ORDER)
-
-# def export_results(results_path, output_path, num_trials, in_order, ssd):
-#     for i in range(num_trials):
-#         validate_path = os.path.join(results_path, compare_path(i + 1, in_order, False, ssd))
-#         assert validate_experiment(validate_path)
-#         if in_order:
-#             validate_path = os.path.join(results_path, compare_path(i + 1, in_order, True, ssd))
-#             assert validate_experiment(validate_path)
-#
-#     path = "experiments.csv"
-#     f = open(path, 'a')
-#
-#     header = "{} | {},".format('In Order' if in_order else 'Out of Order',
-#                                'SSD' if ssd else 'HD') + "," * num_trials + "Plain," + "," * num_trials + "Incremental" + (
-#                  "," * (num_trials + 1) + "Commit\n" if in_order else "\n")
-#     f.write(header)
-#
-#     header = "Resolvers,"
-#     for i in range(num_trials):
-#         header += f"Trial {i + 1},"
-#     header += "Average,"
-#     for i in range(num_trials):
-#         header += f"Trial {i + 1},"
-#     header += "Average"
-#     if in_order:
-#         header += ","
-#         for i in range(num_trials):
-#             header += f"Trial {i + 1},"
-#         header += "Average\n"
-#     else:
-#         header += "\n"
-#
-#     f.write(header)
-#
-#     num_resolvers = 7
-#     for step in range(num_resolvers):
-#         line = f"{step},"
-#
-#         plain_total = 0
-#         for trial in range(num_trials):
-#             plain_path = os.path.join(results_path, experiment_path(trial + 1, False, in_order, False, ssd))
-#             plain_times = job_times(plain_path)
-#
-#             plain_total += plain_times[step]
-#             line += f"{plain_times[step]},"
-#         line += f"{plain_total / num_trials},"
-#
-#         incremental_total = 0
-#         for trial in range(num_trials):
-#             incremental_path = os.path.join(results_path, experiment_path(trial + 1, True, in_order, False, ssd))
-#             incremental_times = job_times(incremental_path)
-#
-#             incremental_total += incremental_times[step]
-#             line += f"{incremental_times[step]},"
-#         line += f"{incremental_total / num_trials}"
-#
-#         if in_order:
-#             line += ","
-#             commit_total = 0
-#             for trial in range(num_trials):
-#                 commit_path = os.path.join(results_path, experiment_path(trial + 1, True, in_order, True, ssd))
-#                 commit_times = job_times(commit_path)
-#
-#                 commit_total += commit_times[step]
-#                 line += f"{commit_times[step]},"
-#             line += f"{commit_total / num_trials}\n"
-#         else:
-#             line += "\n"
-#
-#         f.write(line)
-#     f.write("\n")
-
+        for metric in metrics:
+            write_metric_to_file(f, results_path, num_trials, num_steps, Mode.OUT_OF_ORDER, metric)
+            write_metric_to_file(f, results_path, num_trials, num_steps, Mode.IN_ORDER, metric)
 
 def main():
     parser = argparse.ArgumentParser(description='Parse results of experiment')
@@ -185,21 +115,7 @@ def main():
 
     assert os.path.isdir(results_path)
 
-    export_total_time_experiment(results_path, num_trials, num_steps)
-
-    # export_total_time(results_path, num_trials)
-    # # Out-of-Order SSD
-    # export_results(results_path, output_path, num_trials, False, True)
-    #
-    # # In-Order SSD
-    # export_results(results_path, output_path, num_trials, True, True)
-
-    # Out-of-Order HD
-    # export_results(results_path, output_path, num_trials, False, False)
-
-    # # In-Order HD
-    # export_results(results_path, output_path, num_trials, True, False)
-
+    export_experiments(results_path, num_trials, num_steps)
 
 if __name__ == '__main__':
     main()

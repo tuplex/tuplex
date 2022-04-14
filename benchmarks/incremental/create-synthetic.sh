b33771e05045d235a0845195e59850c044af4143
Ben Givertz <bgivertz@gmail.com>
2022-04-14 13:00:52 -0400

    Synthetic pipeline

--- benchmarks/incremental/create-synthetic.sh
+++ benchmarks/incremental/create-synthetic.sh
@@ -5,7 +5,7 @@ set -x
 python3 synthesize-data.py --dataset-size $1 --output-path /hot/scratch/bgivertz/synthetic/synth0.csv --exceptions 0
 
 for ((i = 1; i <= 9; i++)) do
-  python3 synthesize-data.py --dataset-size $1 --output-path /hot/scratch/bgivertz/synthetic/synth$r.csv --exceptions 0.$r
+  python3 synthesize-data.py --dataset-size $1 --output-path /hot/scratch/bgivertz/synthetic/synth$i.csv --exceptions 0.$i
 done
 
 python3 synthesize-data.py --dataset-size $1 --output-path /hot/scratch/bgivertz/synthetic/synth10.csv --exceptions 1

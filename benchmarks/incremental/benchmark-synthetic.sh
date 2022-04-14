ffb02776b7746256b2666dcfde87c66a524eedb7
Ben Givertz <bgivertz@gmail.com>
2022-04-14 13:20:08 -0400

    Synthetic pipeline

--- benchmarks/incremental/benchmark-synthetic.sh
+++ benchmarks/incremental/benchmark-synthetic.sh
@@ -23,55 +23,55 @@ TIMEOUT=14400
 RESDIR='results_synthetic'
 DATA_PATH='/hot/scratch/bgivertz/data/synthetic/synth'
 INCREMENTAL_OUT_PATH='/hot/scratch/bgivertz/output/incremental'
-INCREMENTAL_COMMIT_OUT_PATH='/hot/scratch/bgivertz/output/commit'
+COMMIT_OUT_PATH='/hot/scratch/bgivertz/output/commit'
 PLAIN_OUT_PATH='/hot/scratch/bgivertz/output/plain'
 
 rm -rf $RESDIR
 rm -rf $INCREMENTAL_OUT_PATH
 rm -rf $PLAIN_OUT_PATH
-rm -rf $INCREMENTAL_COMMIT_OUT_PATH
+rm -rf $COMMIT_OUT_PATH
 
 mkdir -p ${RESDIR}
 
 # create tuplex_config.json
 python3 create_conf.py --opt-pushdown --opt-filter --opt-llvm --executor-count 63 --executor-memory "6G" > tuplex_config.json
 
-echo "running out-of-order ssd experiments"
+echo "running out of order experiments"
 for ((r = 1; r <= NUM_RUNS; r++)); do
   echo "trial ($r/$NUM_RUNS)"
 
   for ((s = 0; s <= 10; s++)) do
-    echo "running plain"
+    echo "running plain ($s/10)"
     LOG="${RESDIR}/plain-out-of-order-e$s-t$r.txt"
     timeout $TIMEOUT ${HWLOC} python3 runsynthetic.py --num-steps $NUM_STEPS --clear-cache --input-path "$DATA_PATH$s.csv" --output-path $PLAIN_OUT_PATH >$LOG 2>$LOG.stderr
 
-    echo "running incremental"
+    echo "running incremental ($s/10)"
     LOG="${RESDIR}/incremental-out-of-order-e$s-t$r.txt"
     timeout $TIMEOUT ${HWLOC} python3 runsynthetic.py --num-steps $NUM_STEPS --clear-cache --incremental-resolution --input-path "$DATA_PATH$s.csv" --output-path $INCREMENTAL_OUT_PATH >$LOG 2>$LOG.stderr
   done
 done
 
-#echo "running in-order ssd experiments"
-#for ((r = 1; r <= NUM_RUNS; r++)); do
-#  echo "trial ($r/$NUM_RUNS)"
-#
-#  echo "running plain"
-#  LOG="${RESDIR}/tuplex-plain-in-order-ssd-$r.txt"
-#  timeout $TIMEOUT ${HWLOC} python3 runtuplex.py --clear-cache --resolve-in-order --path $DATA_PATH_SSD --output-path $PLAIN_OUT_PATH_SSD >$LOG 2>$LOG.stderr
-#
-#  echo "running incremental"
-#  LOG="${RESDIR}/tuplex-incremental-in-order-ssd-$r.txt"
-#  timeout $TIMEOUT ${HWLOC} python3 runtuplex.py --clear-cache --resolve-in-order --incremental-resolution --path $DATA_PATH_SSD --output-path $INCREMENTAL_OUT_PATH_SSD >$LOG 2>$LOG.stderr
-#
-#  echo "running commit"
-#  LOG="${RESDIR}/tuplex-incremental-in-order-commit-ssd-$r.txt"
-#  timeout $TIMEOUT ${HWLOC} python3 runtuplex.py --clear-cache --resolve-in-order --incremental-resolution --commit --path $DATA_PATH_SSD --output-path $INCREMENTAL_COMMIT_OUT_PATH_SSD >$LOG 2>$LOG.stderr
-#
-#done
+echo "running in order experiments"
+for ((r = 1; r < NUM_RUNS; r++)); do
+  echo "trial ($r/$NUM_RUNS)"
 
-#echo "graphing results"
-#python3 graph.py --results-path $RESDIR --num-trials $NUM_RUNS --num-steps 7
+  for ((s = 0; s <= 10; s++)) do
+    echo "running plain ($s/10)"
+    LOG="${RESDIR}/plain-in-order-e$s-t$r.txt"
+    timeout $TIMEOUT ${HWLOC} python3 runtuplex.py --clear-cache --resolve-in-order --input-path "$DATA_PATH$s.csv" --output-path $PLAIN_OUT_PATH >$LOG 2>$LOG.stderr
+
+    echo "running incremental ($s/10)"
+    LOG="${RESDIR}/incremental-in-order-e$s-t$r.txt"
+    timeout $TIMEOUT ${HWLOC} python3 runtuplex.py --clear-cache --resolve-in-order --incremental-resolution --input-path "$DATA_PATH$s.csv" --output-path $INCREMENTAL_OUT_PATH >$LOG 2>$LOG.stderr
+
+
+    echo "running commit ($s/10)"
+    LOG="${RESDIR}/commit-in-order-e$s-t$r.txt"
+    timeout $TIMEOUT ${HWLOC} python3 runtuplex.py --clear-cache --resolve-in-order --incremental-resolution --commit --input-path "$DATA_PATH$s.csv" --output-path $COMMIT_OUT_PATH >$LOG 2>$LOG.stderr
+  done
+done
+
 
 rm -rf $INCREMENTAL_OUT_PATH
 rm -rf $PLAIN_OUT_PATH
-rm -rf $INCREMENTAL_COMMIT_OUT_PATH
+rm -rf $COMMIT_OUT_PATH

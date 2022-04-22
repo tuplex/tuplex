da4def86f72f0c4a008abfdd6d026857f50d6fa9
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-04-22 12:23:52 -0400

    fix

--- scripts/macos/brew_dependencies.sh
+++ scripts/macos/brew_dependencies.sh
@@ -2,8 +2,8 @@
 # install dependencies via brew
 
 brew install coreutils protobuf zstd zlib libmagic llvm@9 pcre2 gflags yaml-cpp celero wget boost
-brew antlr4-cpp-runtime
-brew googletest
+brew install antlr4-cpp-runtime
+brew install googletest
 
 # antlr4-cpp-runtime and googletest have a conflict
 # force googletest

fe23e41bef290fdbfc8a9e7217fa6e0853f566c9
Rhea Goyal <rhea@thanatos.devices.brown.edu>
2022-06-08 16:19:16 -0400

    hm not sure what's happening

--- tuplex/test/core/MathFunctionsTest.cc
+++ tuplex/test/core/MathFunctionsTest.cc
@@ -793,7 +793,7 @@ TEST_F(MathFunctionsTest, MathIsNan) {
         Row(0.0)//, Row(NAN), Row(-3.0), Row(-INFINITY)
     }).map(UDF("lambda x: math.isnan(x)", "", ce)).collectAsVector();
 
-    EXPECT_EQ(v1.size(), 3);
+    EXPECT_EQ(v1.size(), 1);
     EXPECT_EQ(v1[0].getBoolean(0), false);
     // EXPECT_EQ(v1[1].getBoolean(0), true);
     // EXPECT_EQ(v1[2].getBoolean(0), false);

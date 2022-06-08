481faad8119625de834053bd221084af39239818
Rhea Goyal <rhea@thanatos.devices.brown.edu>
2022-06-08 14:26:02 -0400

    added create isnan to .h file

--- tuplex/codegen/include/FunctionRegistry.h
+++ tuplex/codegen/include/FunctionRegistry.h
@@ -189,6 +189,9 @@ namespace tuplex {
                                                       const python::Type &retType,
                                                       const std::vector<tuplex::codegen::SerializableValue> &args);
 
+            SerializableValue createMathIsNanCall(llvm::IRBuilder<>& builder, const python::Type &argsType,
+                                                  const python::Type &retType,
+                                                  const std::vector<tuplex::codegen::SerializableValue> &args);
             // math module functions
             SerializableValue createMathCeilFloorCall(LambdaFunctionBuilder& lfb, llvm::IRBuilder<>& builder, const std::string& qual_name, const SerializableValue& arg);
 

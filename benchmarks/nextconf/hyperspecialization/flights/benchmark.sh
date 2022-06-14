00a03251df268dc2dcb6d69b2f209ec9aa00770f
Your Name <you@example.com>
2022-06-14 08:36:10 -0400

    benchmark file

--- benchmarks/nextconf/hyperspecialization/flights/benchmark.sh
+++ benchmarks/nextconf/hyperspecialization/flights/benchmark.sh
@@ -1,50 +1,31 @@
 #!/usr/bin/env bash
-
-# Parse HWLOC settings
-HWLOC=""
-if [ $# -ne 0 ] && [ $# -ne 1 ]; then # check nmber of inputs
-  echo "usage: ./benchmark.sh [-hwloc]"
-  exit 1
-fi
-
-if [ $# -eq 1 ]; then # check if hwloc
-  if [ "$1" != "-hwloc" ]; then # check flag
-    echo -e "invalid flag: $1\nusage: ./benchmark.sh [-hwloc]"
-    exit 1
-  fi
-  HWLOC="hwloc-bind --cpubind node:1 --membind node:1 --cpubind node:2 --membind node:2"
-fi
+# (c) L.Spiegelberg 2022
+# this file runs the central hyperspecialization vs. no hyperspecialization experiment
 
 # use 11 runs (in case of cold start) and a timeout after 60min
-NUM_RUNS=5
+NUM_RUNS=1
 TIMEOUT=3600
-INPUT_FILE='2019.csv' #'/disk/data/zillow/large10GB.csv'
 
 # make results directory
-RESDIR=results_hyperspecial
+RESDIR=results_hyper
 mkdir -p ${RESDIR}
-
-# basic 
-# compile cpp
-#echo "compiling basic cpp"
-#COMPILE_LOG="${RESDIR}/basic-compile.txt"
-#(time ./build-cpp.sh) &>$COMPILE_LOG
-
-#echo "benchmarking basic"
-#for ((r = 1; r <= NUM_RUNS; r++)); do
-  #LOG="${RESDIR}/basic-run-$r.txt"
-  #timeout $TIMEOUT ${HWLOC} ./basic --path $INPUT_FILE --output_path basic_output >$LOG 2>$LOG.stderr
-#done
-
-# constants 
-# compile cpp
-echo "compiling constant cpp"
-COMPILE_LOG="${RESDIR}/constant-compile.txt"
-(time ./build-cpp.sh) &>$COMPILE_LOG
-
-echo "benchmarking constant"
+PYTHON=python3.9
+echo "benchmarking nohyper (hot)"
 for ((r = 1; r <= NUM_RUNS; r++)); do
-  LOG="${RESDIR}/constant-run-$r.txt"
-  timeout $TIMEOUT ${HWLOC} ./basic --path $INPUT_FILE --output_path constant_output >$LOG 2>$LOG.stderr
+  LOG="${RESDIR}/flights-nohyper-run-$r.txt"
+  echo "running $r/${NUM_RUNS}"
+  timeout $TIMEOUT $PYTHON runtuplex.py --no-hyper >$LOG 2>$LOG.stderr
+  # copy temp aws_job.json result for analysis
+  cp aws_job.json ${RESDIR}/"flights-nohyper-run-$r.json"
 done
+# hyper-specialized
+echo "benchmarking hyper (hot)"
+for ((r = 1; r <= NUM_RUNS; r++)); do
+  LOG="${RESDIR}/flights-hyper-run-$r.txt"
+  echo "running $r/${NUM_RUNS}"
+  timeout $TIMEOUT $PYTHON runtuplex.py >$LOG 2>$LOG.stderr
+  # copy temp aws_job.json result for analysis
+  cp aws_job.json ${RESDIR}/"flights-hyper-run-$r.json"
+done
+echo "done!"
 

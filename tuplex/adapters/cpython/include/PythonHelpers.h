625c209fb112fb87d2f63459dc717c0b619ddfec
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-07-06 22:19:24 -0400

    [BUG] Fixes for key setting and aggregateByKey over arbitrary rows (#112)
    Fix various bugs and improve cmake for more python setups.
    
    Details:
    - Fix for key with optional tuplex. prefixing #69
    - Fix hashing for rows with more complex types
    - Fix running tests on a machine with multiple users by prefixing the scratch dir with the user name
    - Improve python interpreter detection in cmake
    - Remove unused function from test to find python stdlib
    - Fix for missing module, or import failure
    - Add another mod import fallback
    - Add support for typing.Optional[...] type hint and catch errors when extracting type annotations in CSV
    - Add another code extract fallback using new ast.unparse feature for python 3.9+
    - Fix bug94 by making fallback partitions immortal as well
    
    authored-by: Leonhard Spiegelberg <leonhard@brown.edu>

--- tuplex/adapters/cpython/include/PythonHelpers.h
+++ tuplex/adapters/cpython/include/PythonHelpers.h
@@ -100,15 +100,6 @@ namespace python {
     }
 
     /*!
-     * find python standardlib location.
-     * @param version python version string. E.g., "3" or "3.7". Per default search for version build against.
-     * @param prefix_list list of paths where to search for <prefix>/lib/python<version> pattern.
-     * @return string for first match found, empty string else
-     */
-    std::string find_stdlib_location(const std::string& version=python_version(true, false),
-                                     const std::vector<std::string>& prefix_list=std::vector<std::string>{"/usr/local"});
-
-    /*!
      * retrieves main module and loads cloudpickle module. Exits program if cloudpickle is not found.
      * @return module object holding the main module
      */
--- tuplex/test/adapters/cpython/PythonHelperTest.cc
+++ tuplex/test/adapters/cpython/PythonHelperTest.cc
@@ -462,11 +462,7 @@ TEST_F(PythonHelperTest, SchemaEncoding) {
     EXPECT_EQ(python::PyString_AsString(w0), "typing.Callable[[int], str]");
 }
 
-TEST_F(PythonHelperTest, FindStdlib) {
 
+TEST_F(PythonHelperTest, VersionCheck) {
     EXPECT_NE(python::python_version(), "");
-
-    auto loc = python::find_stdlib_location();
-    std::cout<<"Found python stdlib location to be in: "<<loc<<std::endl;
-    EXPECT_NE(loc, "");
 }

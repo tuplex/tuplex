c5cbf05ccb15e5d8f72b8946b57e2292f9c91daa
leonhards <leonhard_spiegelberg@brown.edu>
2022-07-22 11:14:05 -0400

    revert as now proper protobuf has been released

--- scripts/build_macos_wheels.sh
+++ scripts/build_macos_wheels.sh
@@ -12,8 +12,8 @@ cd ..
 # brew tap-new $USER/local-podman
 # brew extract --version=3.19.4 protobuf $USER/local-podman
 # brew install $USER/local-podman/protobuf@3.19.4
-
-export CIBW_BEFORE_BUILD_MACOS="brew tap-new $USER/local; brew extract --force --version=3.19.4 protobuf $USER/local && brew install $USER/local/protobuf@3.19.4 && brew install coreutils zstd zlib libmagic llvm@9 aws-sdk-cpp pcre2 antlr4-cpp-runtime googletest gflags yaml-cpp celero wget boost"
+# i.e., prepend to statemtnt the following: brew tap-new $USER/local; brew extract --force --version=3.19.4 protobuf $USER/local && brew install $USER/local/protobuf@3.19.4 &&
+export CIBW_BEFORE_BUILD_MACOS="brew install protobuf coreutils zstd zlib libmagic llvm@9 aws-sdk-cpp pcre2 antlr4-cpp-runtime googletest gflags yaml-cpp celero wget boost"
 export CIBW_ENVIRONMENT_MACOS="MACOSX_DEPLOYMENT_TARGET=10.13 CMAKE_ARGS='-DBUILD_WITH_AWS=ON -DBUILD_WITH_ORC=ON' "
 
 export CIBW_BUILD="cp3{7,8,9}-*"

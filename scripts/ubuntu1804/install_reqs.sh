fe38ae57f0b60cd5f9881aaff5b9ac532aa81066
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-08-22 15:14:35 +0200

    updated aws sdk for CI

--- scripts/ubuntu1804/install_reqs.sh
+++ scripts/ubuntu1804/install_reqs.sh
@@ -120,7 +120,7 @@ popd &&
 # note for centos7 there's an issue with SSL. Either use aws sdk with -DBUILD_DEPS=ON/-DUSE_OPENSSL=OFF. or force -DUSE_OPENSSL=ON.
 cd /tmp &&
   git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp.git &&
-  cd aws-sdk-cpp && git checkout tags/1.9.133 && mkdir build && pushd build &&
+  cd aws-sdk-cpp && git checkout tags/1.9.328 && mkdir build && pushd build &&
   cmake -DCMAKE_BUILD_TYPE=Release -DUSE_OPENSSL=ON -DENABLE_TESTING=OFF -DENABLE_UNITY_BUILD=ON -DCPP_STANDARD=14 -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="s3;core;lambda;transfer" -DCMAKE_INSTALL_PREFIX=/opt .. &&
   make -j32 &&
   make install &&

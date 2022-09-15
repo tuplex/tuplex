78478a343afaa27fd9ea7edbdca88f58bacab842
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-09-14 22:14:53 -0400

    another try

--- scripts/macos/install_antlr4_cpp_runtime.sh
+++ scripts/macos/install_antlr4_cpp_runtime.sh
@@ -7,7 +7,13 @@ PREFIX=/usr/local
 git clone https://github.com/antlr/antlr4.git \
 && cd antlr4 && cd runtime &&  git fetch --all --tags \
 && git checkout tags/4.9.3 -b 4.9.3 && cd Cpp/ && ./deploy-macos.sh \
-&& unzip antlr4-cpp-runtime-macos.zip \
-&& cd lib && mv * $PREFIX/lib/ && cd .. \
+&& unzip -l antlr4-cpp-runtime-macos.zip && unzip antlr4-cpp-runtime-macos.zip \
+&& cd lib && cp -R * $PREFIX/lib/ && cd .. \
 && mv antlr4-runtime $PREFIX/include/ \
 && echo "ANTLR4 Cpp runtime installed to $PREFIX"
+
+# execute copy command (fix for delocate wheel)
+ls -l /usr/local/include
+ls -l /usr/local/lib
+
+cp lib/libantlr4-runtime.dylib /Users/runner/work/tuplex/tuplex/libantlr4-runtime.dylib

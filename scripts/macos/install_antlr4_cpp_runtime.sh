5a17859925faa14ff6c38bb826ac3a3c6b0e54a3
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-09-14 22:57:04 -0400

    update

--- scripts/macos/install_antlr4_cpp_runtime.sh
+++ scripts/macos/install_antlr4_cpp_runtime.sh
@@ -4,6 +4,9 @@
 # specify here target location
 PREFIX=/usr/local
 
+# if antlr4 exists already, skip
+[ -d "antlr4" ] && exit 0
+
 git clone https://github.com/antlr/antlr4.git \
 && cd antlr4 && cd runtime &&  git fetch --all --tags \
 && git checkout tags/4.9.3 -b 4.9.3 && cd Cpp/ && ./deploy-macos.sh \
@@ -17,3 +20,5 @@ ls -l /usr/local/include
 ls -l /usr/local/lib
 
 cp lib/libantlr4-runtime.dylib /Users/runner/work/tuplex/tuplex/libantlr4-runtime.dylib
+
+exit 0

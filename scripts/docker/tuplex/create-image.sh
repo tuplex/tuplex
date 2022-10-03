bf2f5e116d99bcc5befe8ead4013df1d3b3cb054
Your Name <you@example.com>
2022-10-03 14:25:00 -0400

    version update & docker update

--- scripts/docker/tuplex/create-image.sh
+++ scripts/docker/tuplex/create-image.sh
@@ -20,10 +20,10 @@ cp -R ../../../examples/sample_data .
 # build benchmark docker image
 # copy from scripts to current dir because docker doesn't understand files
 # outside the build context
-docker build -t tuplex/tuplex:v0.3.3 -f Dockerfile . || exit 1
+docker build -t tuplex/tuplex:v0.3.4 -f Dockerfile . || exit 1
 
 # is upload set?
 if [[ "${UPLOAD}" == 'SET' ]]; then
   docker login
-  docker push tuplex/tuplex:v0.3.3
+  docker push tuplex/tuplex:v0.3.4
 fi
--- scripts/set_version.py
+++ scripts/set_version.py
@@ -15,7 +15,7 @@ def LooseVersion(v):
 
 
 # to create a testpypi version use X.Y.devN
-version = '0.3.4'
+version = '0.3.5dev'
 
 # https://pypi.org/simple/tuplex/
 # or https://test.pypi.org/simple/tuplex/

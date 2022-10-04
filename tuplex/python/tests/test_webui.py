3e521901516eb545cc5900e7252babe9b20ef3e0
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-10-04 11:50:12 -0400

    change localhost to 127.0.0.1

--- tuplex/python/tests/test_webui.py
+++ tuplex/python/tests/test_webui.py
@@ -22,9 +22,13 @@ class TestWebUI(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+        localhost_ip = '127.0.0.1'
+
         # bug in logging redirect?
         conf ={"webui.enable": True, "driverMemory": "8MB", "executorMemory" : "1MB",
-               "partitionSize": "256KB", "tuplex.redirectToPythonLogging": True}
+               "partitionSize": "256KB", "tuplex.redirectToPythonLogging": True,
+               "webui.mongodb.url" : localhost_ip, "webui.url" : localhost_ip}
 
         logging.debug('WebUI Test setUpClass called')
         cls.context = Context(conf)
@@ -34,10 +38,10 @@ class TestWebUI(unittest.TestCase):
     def tearDownClass(cls) -> None:
         logging.debug('WebUI Test tearDownClass called')
         del cls.context
+        cls.context = None
 
         # shutdown processes manually!
         auto_shutdown_all()
-
 
     # check connection to WebUI works
     def test_webuiconnect(self):

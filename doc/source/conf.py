90add3557af4b9f5eb0cfbc62c5c784e1c27cf37
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-10-05 13:40:04 -0400

    version bump

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,7 +36,7 @@ author = 'Leonhard Spiegelberg'
 # The short X.Y version
 version="0.3"
 # The full version, including alpha/beta/rc tags
-release="0.3.5"
+release="0.3.6dev"
 
 
 # -- General configuration ---------------------------------------------------
--- tuplex/python/setup.py
+++ tuplex/python/setup.py
@@ -29,7 +29,7 @@ files = list(filter(lambda x: '__pycache__' not in x and not x.endswith('.pyc'),
 
 setup(
     name="Tuplex",
-    version="0.3.5",
+    version="0.3.6dev",
     packages=find_packages(),
     package_data={
       # include libs in libexec

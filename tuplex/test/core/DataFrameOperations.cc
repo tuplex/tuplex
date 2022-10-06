3ca9d9b283cd5ce031d1ddaa28842eb8fa39c603
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-10-06 15:19:31 -0400

    swapping test file

--- tuplex/test/core/DataFrameOperations.cc
+++ tuplex/test/core/DataFrameOperations.cc
@@ -89,7 +89,7 @@ TEST_F(CSVDataFrameTest, SimpleMapColumnI) {
     using namespace tuplex;
 
     auto v = csvfile().mapColumn("firstname", UDF("lambda fn: fn[0]"))
-                      .mapColumn("income", UDF("lambda x: x / 1000"))s
+                      .mapColumn("income", UDF("lambda x: x / 1000"))
                       .collectAsVector();
     ASSERT_EQ(v.size(), 2);
     printRows(v);

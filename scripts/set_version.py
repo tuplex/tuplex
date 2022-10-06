8d9e8904c9921fbce75c8f0c9ed8a2c34a8fe6de
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-10-06 12:40:21 -0400

    [FEATURE] Version bump to 0.3.6dev (#134)
    Version bump from 0.3.5/0.3.5dev to 0.3.6dev.
    
    authored-by: Leonhard Spiegelberg <leonhard@brown.edu>

--- scripts/set_version.py
+++ scripts/set_version.py
@@ -17,7 +17,7 @@ def LooseVersion(v):
 
 
 # to create a testpypi version use X.Y.devN
-version = '0.3.5'
+version = '0.3.6dev'
 
 # https://pypi.org/simple/tuplex/
 # or https://test.pypi.org/simple/tuplex/

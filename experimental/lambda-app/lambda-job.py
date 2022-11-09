0f196c44449074ff291694083e8635df9f95e687
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-11-08 23:19:04 -0500

    chartung

--- experimental/lambda-app/lambda-job.py
+++ experimental/lambda-app/lambda-job.py
@@ -7,12 +7,17 @@ import pandas as pd
 import json
 import os
 import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_col = np.array([53, 66, 89]) / 255.0#sns.color_palette()[0]
+g_col = np.array([140, 192, 222]) / 255.0 #sns.color_palette()[1]
 
 
 DATA_PATH='/Users/leonhards/projects/tuplex-public/tuplex/cmake-build-debug/dist/bin/aws_job.json'
-
 data = json.loads(open(DATA_PATH, 'r').read())
 
+# st.set_page_config(page_title="Lambda job explore app", layout='wide')
 st.title('Lambda Job overview')
 
 
@@ -20,7 +25,7 @@ stage_runtime_in_s = (data['stageEndTimestamp'] - data['stageStartTimestamp']) /
 col1, col2, col3 = st.columns(3)
 col1.metric('Runtime', '{:.2f}s'.format(stage_runtime_in_s), help='how long the job took to run')
 col2.metric('Hyperspecialization', 'On' if data['hyper_mode'] else 'Off')
-#col3.metric('Cost', '${:.2f}s'.format(data['cost']), help='total cost to run (incl. S3)')
+col3.metric('Cost', '${:.2f}'.format(data['cost']), help='total cost to run (incl. S3)')
 
 st.write('input paths rows took:')
 # write out input paths rows took
@@ -60,3 +65,44 @@ out_df = pd.DataFrame({
     'unresolved': [out_unresolved],
 })
 st.table(out_df)
+st.markdown("""---""")
+
+
+def requests_to_pyplot(requests, ts_start):
+    x, y1, y2 = [], [], []
+    x = np.arange(len(requests))
+    y1, y2 = np.zeros(x.shape), np.zeros(x.shape)
+
+    for i, req in enumerate(requests):
+        start, end = pd.to_datetime(req['tsRequestStart']), pd.to_datetime(req['tsRequestEnd'])
+        y1[i] = (end - ts_start).total_seconds()
+        y2[i] = (start - ts_start).total_seconds()
+    return x, y1, y2
+
+def plot_request_completion_chart(st, requests, ts_start):
+    sort_key = 'tsRequestEnd'
+    sorted_reqs = sorted(requests, key=lambda x: x[sort_key])
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    x, y1, y2 = requests_to_pyplot(sorted_reqs, ts_start)
+    # pct
+    x = x / (len(x) -1) * 100.0
+    #label = 'hyper $\\!$ ($\\mu={:.1f}\\mathrm{{s}}$, \\, $\\sigma={:.1f}\\mathrm{{s}}$, \\, $\\${:.2f}$)'.format(hyper_mu, hyper_sigma, hyper_cost)
+    plt.fill_between(x, y1, y2, alpha=1.0, color=n_col, linewidth=2)
+    plt.grid(axis='x')
+    #plt.legend(loc='upper left', fontsize=24)
+    plt.ylabel('time in s')
+    plt.xlabel('% of requests completed')
+    plt.ylim(0, 75)
+    plt.xlim(0, 100)
+
+    st.pyplot(fig)
+
+
+ts_start = pd.to_datetime(data['stageStartTimestamp'])
+requests = data['requests']
+col1, col2 = st.columns(2)
+col1.metric('Requests', len(requests), help='number of requests')
+plot_request_completion_chart(col2, requests, ts_start)
+

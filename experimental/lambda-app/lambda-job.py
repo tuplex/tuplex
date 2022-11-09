325392762d8f559eada2af57124657b0045a4cd7
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-11-09 02:32:20 -0500

    parallelism study

--- experimental/lambda-app/lambda-job.py
+++ experimental/lambda-app/lambda-job.py
@@ -135,3 +135,63 @@ df_reqs = pd.DataFrame(rows)
 st.write('Individual requests overview:')
 st.dataframe(df_reqs)
 
+
+# check which containers requests are assigned to.
+# -> this will allow to deduce parallelism!
+num_containers = len(set([req['containerId'] for req in sorted_reqs]))
+
+# count how many requests are active at timepoint t
+def active_requests(requests, t, ts_start):
+   # request is active iff ts_start + t falls between request start / end
+    def is_active(req, ts_start, t):
+        start, end = pd.to_datetime(req['tsRequestStart']), pd.to_datetime(req['tsRequestEnd'])
+        if end <= ts_start:
+            return False
+        return (end - ts_start).total_seconds() >= t
+
+    active_requests = [req for req in requests if is_active(req, ts_start, t)]
+    return active_requests
+
+# use request end time (relative to ts_start)
+x = [0.0] +  [(pd.to_datetime(req['tsRequestStart']) - ts_start).total_seconds() for req in sorted_reqs] + [(pd.to_datetime(req['tsRequestEnd']) - ts_start).total_seconds() for req in sorted_reqs] + [stage_runtime_in_s]
+x = sorted(x)
+
+def nunique_containers(reqs):
+    return len(set([req['containerId'] for req in reqs]))
+
+y = [nunique_containers(active_requests(requests, t, ts_start)) for t in x]
+
+# display metrics
+max_parallelism = np.array(y).max()
+containers = [t['container'] for t in data['tasks']]
+nreused = []
+nnew = []
+for c in containers:
+    if c['reused']:
+        nreused.append(c['uuid'])
+    else:
+        nnew.append(c['uuid'])
+nreused = len(set(nreused))
+nnew = len(set(nnew))
+col1, col2, col3, col4 = st.columns(4)
+col1.metric('Unique containers (total)', num_containers, help='each container corresponds to a Lambda executor')
+col2.metric('Max parallelism', max_parallelism, help='Maximum amount of containers active at any time')
+col3.metric('Reused containers', nreused, help="number of reused Lambda executors, from previous run")
+col4.metric('New containers', nnew, help='number of newly instantiated Lambda executors')
+
+
+# create figure
+fig, ax = plt.subplots(figsize=(6,6))
+ax.plot([0.0] + x, [0.0] + y, '-x')
+ax.set_xlabel('time in s')
+ax.set_ylabel('active containers')
+col1, col2 = st.columns(2)
+col1.pyplot(fig)
+
+# could use this here to analyze quickly hyper/general jobs etc.
+# options = st.multiselect(
+#     'What are your favorite colors',
+#     ['Green', 'Yellow', 'Red', 'Blue'],
+#     ['Yellow', 'Red'])
+#
+# st.write('You selected:', options)

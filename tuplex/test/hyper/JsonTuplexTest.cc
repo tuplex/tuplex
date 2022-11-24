e5a181f761a1bcab70aa516b26b1ddb1c30e716c
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2022-11-23 20:43:33 -0500

    update

--- tuplex/test/hyper/JsonTuplexTest.cc
+++ tuplex/test/hyper/JsonTuplexTest.cc
@@ -106,18 +106,18 @@ TEST_F(JsonTuplexTest, BasicLoad) {
     // 2, 42, 12.0      <-- note the order correction
     // None, None, 2    <-- note the order (fallback/general case)
     unwrap_first_level = true;
-//     ctx.json("../resources/ndjson/example2.json", unwrap_first_level).show();
-//    auto v = ctx.json("../resources/ndjson/example2.json", unwrap_first_level).collectAsVector();
-//    ASSERT_EQ(v.size(), 3);
-//    EXPECT_EQ(v[0].toPythonString(), "(10,None,3.41400)");
-//    EXPECT_EQ(v[1].toPythonString(), "(2,42,12.00000)");
-//    EXPECT_EQ(v[2].toPythonString(), "(None,None,2.00000)");
+     ctx.json("../resources/ndjson/example2.json", unwrap_first_level).show();
+    auto v = ctx.json("../resources/ndjson/example2.json", unwrap_first_level).collectAsVector();
+    ASSERT_EQ(v.size(), 3);
+    EXPECT_EQ(v[0].toPythonString(), "(10,None,3.41400)");
+    EXPECT_EQ(v[1].toPythonString(), "(2,42,12.00000)");
+    EXPECT_EQ(v[2].toPythonString(), "(None,None,2.00000)");
 
 
-    unwrap_first_level = false; // --> requires implementing/adding decoding of struct dict...
-    ctx.json("../resources/ndjson/example2.json", unwrap_first_level).show();
-    // // large json file (to make sure buffers etc. work
-    // ctx.json("/data/2014-10-15.json").show();
+//    unwrap_first_level = false; // --> requires implementing/adding decoding of struct dict...
+//    ctx.json("../resources/ndjson/example2.json", unwrap_first_level).show();
+//    // // large json file (to make sure buffers etc. work
+//    // ctx.json("/data/2014-10-15.json").show();
 }
 
 TEST_F(JsonTuplexTest, BasicLoadWithSlowDecode) {

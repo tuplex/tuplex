dcd4d4271ea3315199f7def7ef20011ef85c7eff
Leonhard Spiegelberg <leonhard@brown.edu>
2023-04-15 16:05:48 -0700

    lithops benchmark

--- benchmarks/nextconf/lithops/benchmark.sh
+++ benchmarks/nextconf/lithops/benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# (c) L.Spiegelberg 2023
+# (c) L.Spiegelberg 2022
 # this file runs the pure python mode for the filter query (to establish a baseline!)
 
 # use 11 runs (in case of cold start) and a timeout after 60min
@@ -9,27 +9,27 @@ TIMEOUT=3600
 #-------------------------------------------------------------------------------------------------
 # set 1: internal format
 # make results directory
-RESDIR=experimental_results/flights/baseline
+RESDIR=experimental_results/lithops/flights
 echo "Storing experimental result in ${RESDIR}, using ${NUM_RUNS} runs"
 mkdir -p ${RESDIR}
 PYTHON=python3.9
 
-# rm job folder if it exists...
-[ -d job ] && rm -rf job
-
-echo "Benchmark flights in pure python mode (baseline)"
+echo "Benchmark flights in custom mode (Lithops)"
 for ((r = 1; r <= NUM_RUNS; r++)); do
-  LOG="${RESDIR}/flights-python-run-$r.txt"
+  LOG="${RESDIR}/flights-lithops-custom-run-$r.txt"
   echo "running $r/${NUM_RUNS}"
-  timeout $TIMEOUT $PYTHON runtuplex-filter.py --python-mode >$LOG 2>$LOG.stderr
-  # copy temp aws_job.json result for analysis
-  cp aws_job.json ${RESDIR}/"flights-python-run-$r.json"
+  timeout $TIMEOUT $PYTHON flights_query.py --mode custom >$LOG 2>$LOG.stderr
 done
-# mv job folder
-mkdir -p $RESDIR/flights-python
-mv job/*.json $RESDIR/flights-python
-rm -rf job
 
+echo "Benchmark flights in Viton mode (Lithops)"
+for ((r = 1; r <= NUM_RUNS; r++)); do
+  LOG="${RESDIR}/flights-lithops-viton-run-$r.txt"
+  echo "running $r/${NUM_RUNS}"
+  timeout $TIMEOUT $PYTHON flights_query.py --mode viton >$LOG 2>$LOG.stderr
+done
+
+# move the lithops plots
+mv plots ${RESDIR}
 
 echo "done!"
 

6e432a3938518cdaadf2d3994da5b8e4ed09834b
Leonhard Spiegelberg <leonhard@brown.edu>
2023-07-06 14:57:17 -0700

    add antlr4 runtime version extraction

--- tuplex/cmake/FindANTLR4Runtime.cmake
+++ tuplex/cmake/FindANTLR4Runtime.cmake
@@ -16,8 +16,20 @@ find_library(ANTLR4Runtime_LIB antlr4-runtime)
 
 set(ANTLR4Runtime_VERSION ${PC_ANTLR4Runtime_VERSION})
 
+# version empty? read from header file
+if(NOT ANTLR4Runtime_VERSION MATCHES [0-9]+.[0-9]+.[0-9]+)
+    set(ANTLR4Runtime_VERSION_FILE "${ANTLR4Runtime_INCLUDE_DIR}/Version.h")
+    file(READ ${ANTLR4Runtime_VERSION_FILE} FILE_CONTENTS)
+    string(REGEX MATCH "VERSION_MAJOR ([0-9]*)" _ ${FILE_CONTENTS})
+    set(ver_major ${CMAKE_MATCH_1})
+    string(REGEX MATCH "VERSION_MINOR ([0-9]*)" _ ${FILE_CONTENTS})
+    set(ver_minor ${CMAKE_MATCH_1})
+    string(REGEX MATCH "VERSION_PATCH ([0-9]*)" _ ${FILE_CONTENTS})
+    set(ver_patch ${CMAKE_MATCH_1})
+    set(ANTLR4Runtime_VERSION "${ver_major}.${ver_minor}.${ver_patch}")
+endif()
 
-mark_as_advanced(ANTLR4Runtime_FOUND ANTLR4Runtime_INCLUDE_DIR ANTLR4Runtime_LIB ANTLR4Runtime_VERSION)
+mark_as_advanced(ANTLR4Runtime_FOUND ANTLR4Runtime_INCLUDE_DIR ANTLR4Runtime_LIB ANTLR4Runtime_VERSION ANTLR4Runtime_VERSION)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ANTLR4Runtime

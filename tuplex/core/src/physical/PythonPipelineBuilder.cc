64a8cb1a0a4e6986fb147a6bd41057906170f5f0
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2023-08-28 23:41:50 -0700

    fix rename logic

--- tuplex/core/src/physical/PythonPipelineBuilder.cc
+++ tuplex/core/src/physical/PythonPipelineBuilder.cc
@@ -737,40 +737,43 @@ void PythonPipelineBuilder::cellInput(int64_t operatorID, std::vector<std::strin
         }
 
         auto key_column_idx = std::max(left_key_idx, right_key_idx);
+        auto key_column = _lastColumns[key_column_idx];
 
-        // @TODO: prefixing?
+        key_column = leftColumn.value_or(rightColumn.value_or(""));
 
+        auto build_right = right_key_idx >= 0; // because always the "left" column is taken, can infer build direction
         std::__1::vector<std::string> result_columns;
+        if(build_right) {
+            // the bucket columns come first
+            std::__1::transform(bucketColumns.begin(), bucketColumns.end(), std::back_inserter(result_columns),
+                                [&](const std::string& name) { return leftPrefix.value_or("") + name + leftSuffix.value_or("");});
+            result_columns.push_back(key_column); // no prefixing for key column
 
-        if(left_key_idx >= 0) {
+            // the other columns come first
             for(unsigned i = 0; i < _lastColumns.size(); ++i) {
-                if(i != left_key_idx)
+                if(i != key_column_idx)
+                    result_columns.push_back(rightPrefix.value_or("") + _lastColumns[i] + rightSuffix.value_or(""));
+            }
+        } else {
+            // the other columns come first
+            for(unsigned i = 0; i < _lastColumns.size(); ++i) {
+                if(i != key_column_idx)
                     result_columns.push_back(leftPrefix.value_or("") + _lastColumns[i] + leftSuffix.value_or(""));
             }
-            result_columns.push_back(_lastColumns[left_key_idx]); // no prefixing for key column
-        } else {
-            // use all left columns (key is on right side!)
-            result_columns = _lastColumns;
+            result_columns.push_back(key_column); // no prefixing for key column
+
+            if(right_key_idx >= 0)
+                result_columns.push_back(_lastColumns[right_key_idx]);
+            std::__1::transform(bucketColumns.begin(), bucketColumns.end(), std::back_inserter(result_columns),
+                                [&](const std::string& name) { return rightPrefix.value_or("") + name + rightSuffix.value_or("");});
         }
-
-//        if(right_key_idx)
-
-//        // copy columns from start to key_column_idx (excl)
-//        std::copy(_lastColumns.begin(), _lastColumns.begin() + key_column_idx, std::back_inserter(result_columns));
-//        std::copy(_lastColumns.begin() + key_column_idx + 1, _lastColumns.end(), std::back_inserter(result_columns));
-//        // prefix with left side
-//        for(auto& name : _lastColumns)
-//            name = leftPrefix.value_or("") + name + leftSuffix.value_or("");
-//
-//        auto key_column = _lastColumns[key_column_idx]; // <-- ??
-//
-//        result_columns.push_back(key_column); // <-- no prefixing
-        std::__1::transform(bucketColumns.begin(), bucketColumns.end(), std::back_inserter(result_columns),
-                            [&](const std::string& name) { return rightPrefix.value_or("") + name + rightSuffix.value_or("");});
 
         // update the key column projection pair
         // map is original column -> projected column
         if(!_lastProjectionMap.empty()) {
+
+            // TODO: need to update with previous column assignment...
+
             _lastProjectionMap = transform_pairs<int,int>(_lastProjectionMap,
                                                           [&](const std::pair<int,int>& pair) -> std::pair<int,int> {
                                                               if(pair.first == key_column_idx) {

cbc27ccd78866e81b20e5a1b0a6b0be8f6357b35
Leonhard Spiegelberg <leonhard@brown.edu>
2023-10-10 18:57:08 -0700

    remove old scripts, update aws sdk script for cibuildwheel

--- scripts/macos/install_aws-sdk-cpp.sh
+++ scripts/macos/install_aws-sdk-cpp.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-echo ">> installing AWS SDK from source"
-CPU_CORES=$(sysctl -n hw.physicalcpu)
+AWSSDK_CPP_VERSION=1.11.164
+
+echo ">> installing AWS SDK ${AWSSDK_CPP_VERSION} from source"
+CPU_COUNT=$(sysctl -n hw.physicalcpu)
+echo "-- building with ${CPU_COUNT} cores"
 
 # if macOS is 10.x -> use this as minimum
 MINIMUM_TARGET="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.13"
@@ -21,12 +24,12 @@ else
     echo "defaulting build to use as minimum target ${MINIMUM_TARGET}"
 fi
 
-cd /tmp &&
-  git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp.git &&
-  cd aws-sdk-cpp && git checkout tags/1.9.320 && mkdir build && pushd build &&
-  cmake ${MINIMUM_TARGET} -DCMAKE_BUILD_TYPE=Release -DUSE_OPENSSL=ON -DENABLE_TESTING=OFF -DENABLE_UNITY_BUILD=ON -DCPP_STANDARD=14 -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="s3;core;lambda;transfer" .. &&
-  make -j${CPU_CORES} &&
-  make install &&
+cd /tmp \
+  && git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp.git \
+  && cd aws-sdk-cpp && git checkout tags/${AWSSDK_CPP_VERSION} && sed -i 's/int ret = Z_NULL;/int ret = static_cast<int>(Z_NULL);/g' src/aws-cpp-sdk-core/source/client/RequestCompression.cpp && mkdir build && cd build \
+  && cmake ${MINIMUM_TARGET} -DCMAKE_BUILD_TYPE=Release -DUSE_OPENSSL=ON -DENABLE_TESTING=OFF -DENABLE_UNITY_BUILD=ON -DCPP_STANDARD=17 -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="s3;core;lambda;transfer" -DCMAKE_INSTALL_PREFIX=${PREFIX} .. \
+  && make -j ${CPU_COUNT} \
+  && make install &&
   popd &&
   cd - || echo ">> error: AWS SDK failed"
 

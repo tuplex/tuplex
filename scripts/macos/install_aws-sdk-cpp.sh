6d1c93888c3a252f8683a0df2ce2ad4ea8698b64
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2023-10-10 19:17:56 -0700

    improving aws sdk

--- scripts/macos/install_aws-sdk-cpp.sh
+++ scripts/macos/install_aws-sdk-cpp.sh
@@ -13,10 +13,10 @@ MACOS_VERSION=$(sw_vers -productVersion)
 echo "-- processing on MacOS ${MACOS_VERSION}"
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
-MACOS_VERSION_MAJOR=${MACOS_VERSION%.*}
-if [ $MACOS_VERSION_MAJOR -ge 11 ]; then
+MACOS_VERSION_MAJOR=`echo $MACOS_VERSION | cut -d . -f1`
+
+if [ "$MACOS_VERSION_MAJOR" -ge 11 ]; then
     echo "-- Newer MacOS detected (>=11.0), using more recent base target."
-    MACOS_VERSION_MAJOR=${MACOS_VERSION%.*}
     echo "-- Using minimum target ${MACOS_VERSION_MAJOR}.0"
     MINIMUM_TARGET="-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOS_VERSION_MAJOR}.0"
 else
@@ -26,7 +26,7 @@ fi
 
 cd /tmp \
   && git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp.git \
-  && cd aws-sdk-cpp && git checkout tags/${AWSSDK_CPP_VERSION} && sed -i 's/int ret = Z_NULL;/int ret = static_cast<int>(Z_NULL);/g' src/aws-cpp-sdk-core/source/client/RequestCompression.cpp && mkdir build && cd build \
+  && cd aws-sdk-cpp && git checkout tags/${AWSSDK_CPP_VERSION} && sed -i '' 's/int ret = Z_NULL;/int ret = static_cast<int>(Z_NULL);/g' src/aws-cpp-sdk-core/source/client/RequestCompression.cpp && mkdir build && cd build \
   && cmake ${MINIMUM_TARGET} -DCMAKE_BUILD_TYPE=Release -DUSE_OPENSSL=ON -DENABLE_TESTING=OFF -DENABLE_UNITY_BUILD=ON -DCPP_STANDARD=17 -DBUILD_SHARED_LIBS=OFF -DBUILD_ONLY="s3;core;lambda;transfer" -DCMAKE_INSTALL_PREFIX=${PREFIX} .. \
   && make -j ${CPU_COUNT} \
   && make install &&

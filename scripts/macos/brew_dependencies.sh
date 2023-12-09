67d10cbf85e27cb78da752730660af96318b4f28
Leonhard Spiegelberg <leonhard@brown.edu>
2023-12-08 18:36:55 -0800

    llvm downgrade

--- scripts/macos/brew_dependencies.sh
+++ scripts/macos/brew_dependencies.sh
@@ -2,4 +2,6 @@
 # This script installs all required dependencies via brew
 # for instructions on how to install brew, visit https://brew.sh/
 
-brew install openjdk@11 cmake coreutils protobuf zstd zlib libmagic llvm@16 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf
+
+# brew doesn't provide llvm@16 bottle anymore for big sur, but python3.8 only works with big sur tags. use llvm@15 instead
+brew install openjdk@11 cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf

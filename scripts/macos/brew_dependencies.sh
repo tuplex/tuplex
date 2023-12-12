3e1c154d379cf4d2a1073d9ca139106fd5c9ebac
Leonhard Spiegelberg <leonhard@brown.edu>
2023-12-11 21:01:29 -0800

    add linking step for cache

--- scripts/macos/brew_dependencies.sh
+++ scripts/macos/brew_dependencies.sh
@@ -5,3 +5,6 @@
 
 # brew doesn't provide llvm@16 bottle anymore for big sur, but python3.8 only works with big sur tags. use llvm@15 instead
 brew install openjdk@11 cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf
+
+# link (when e.g. used from restoring cache)
+brew link --overwrite cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf

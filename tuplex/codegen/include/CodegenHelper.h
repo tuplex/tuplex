f7db3a22384b8b4ed0c42fab8a5ca0c142872755
Leonhard Spiegelberg <leonhard@brown.edu>
2023-12-12 22:42:58 -0800

    cleanup

--- tuplex/codegen/include/CodegenHelper.h
+++ tuplex/codegen/include/CodegenHelper.h
@@ -763,14 +763,7 @@ namespace tuplex {
                 llvm::Instruction& inst = *firstBlock.getFirstInsertionPt();
                 ctorBuilder.SetInsertPoint(&inst);
             }
-// disable here clang/gcc warning just for this - it's a limitation of how ctorbuilder is architected.
-// #pragma clang diagnostic push
-// #pragma clang diagnostic ignored "-Wreturn-local-addr"
-// #pragma GCC diagnostic push
-// #pragma GCC diagnostic ignored "-Wreturn-local-addr"
             return std::move(ctorBuilder);
-// #pragma GCC diagnostic pop
-// #pragma clang diagnostic pop
         }
 
         // in order to serialize/deserialize data properly and deal with

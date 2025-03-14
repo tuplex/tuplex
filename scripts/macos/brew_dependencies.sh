11dc83fbc9a679a9af9f5a53e9a19000438d9421
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2025-03-13 18:54:09 -0700

    brewed aws sdk to check whether compatible with pyarrow

--- scripts/macos/brew_dependencies.sh
+++ scripts/macos/brew_dependencies.sh
@@ -2,9 +2,8 @@
 # This script installs all required dependencies via brew
 # for instructions on how to install brew, visit https://brew.sh/
 
-
 # brew doesn't provide llvm@16 bottle anymore for big sur, but python3.8 only works with big sur tags. use llvm@15 instead
-brew install openjdk@11 cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf
+brew install openjdk@11 cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf aws-sdk-cpp
 
 # link (when e.g. used from restoring cache)
-brew link --overwrite cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf abseil
+brew link --overwrite cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf abseil aws-sdk-cpp

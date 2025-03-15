f8e0ba60f720c1e8a4faacbc2ee6cc28c07f42f0
Leonhard Spiegelberg <leonhard_spiegelberg@brown.edu>
2025-03-15 15:06:17 -0700

    fix

--- scripts/macos/brew_dependencies.sh
+++ scripts/macos/brew_dependencies.sh
@@ -2,8 +2,9 @@
 # This script installs all required dependencies via brew
 # for instructions on how to install brew, visit https://brew.sh/
 
+
 # brew doesn't provide llvm@16 bottle anymore for big sur, but python3.8 only works with big sur tags. use llvm@15 instead
-brew reinstall openjdk@11 cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf aws-sdk-cpp
+brew install openjdk@11 cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf
 
 # link (when e.g. used from restoring cache)
-brew link --overwrite cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf abseil aws-sdk-cpp
+brew link --overwrite cmake coreutils protobuf zstd zlib libmagic llvm@15 pcre2 gflags yaml-cpp celero wget boost googletest libdwarf libelf abseil

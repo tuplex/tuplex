0acd57490801ce6eb7d4da8203d6c9fc473175f3
Leonhard Spiegelberg <leonhard.spiegelberg@gmail.com>
2025-07-15 18:04:34 -0700

    [CI] Orc version update for cmake v4 (#157)
    Update ORC dependency to 2.1.3, snappy and zstd. Fix build errors caused by cmake v4. Set minimum macOS version supported to 13.6.

--- tuplex/cmake/ExternalAntlr4Cpp.cmake
+++ tuplex/cmake/ExternalAntlr4Cpp.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.12 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.20 FATAL_ERROR)
 
 if(${CMAKE_VERSION} VERSION_GREATER "3.19.0")
   cmake_policy(SET CMP0114 OLD) # policy update in 3.19, explicitly use old behavior
